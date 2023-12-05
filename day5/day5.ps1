bae6e3ac0c23b9060d0a07972195dfe72c1d15d9
SGA-JasonWridgway <jason.wridgway@safegroup.com.au>
2023-12-05 20:57:42 +1100

    Working on day 5

---
+++ day5/day5.ps1
@@ -0,0 +1,70 @@
+#Get input
+#$inputSource = "$PSScriptRoot/example.txt" #Answer should be 35 for part 1
+$inputSource = "$PSScriptRoot/input.txt"
+$inputText = Get-Content $inputSource
+
+<#
+      Part 1
+    ----------
+    Get Seeds
+    Get maps
+    Convert to mapping functions in an array
+    Walk each seed through array of mapping functions
+    Select minimum
+#>
+$seeds = ($inputText[0] | Select-String '.*: (\d+ *)+').Matches.Groups[1].Captures.Value.Trim()
+
+Clear-Variable maps
+$maps = @(@{})
+
+$range = 3..($inputText.Count-1)
+$inputText[$range] | ForEach-Object{
+    if($_ -match 'map:'){
+        $maps += @{}
+    }
+    elseif($_ -ne ''){
+
+        #####this needs to be maths, not preallocated array
+        $destStart, $sourceStart, $mapLen = $_.Trim() -split ' '
+        for ($j = 0; $j -lt $mapLen; $j++) {
+            $maps[-1].Add([string]([long]$sourceStart + $j), [string]([long]$destStart + $j))
+        }
+    }
+}
+$locations = $seeds | ForEach-Object{
+    $currVal = $_
+    $maps | ForEach-Object{
+        $map = $_
+        $currVal = $map.ContainsKey("$currVal") ? $map["$currVal"] : $currVal
+    }
+    $currVal
+}
+$part1 = ($locations | measure -Minimum).Minimum
+write-host "Part 1: $part1"
+
+
+
+
+
+# $rawMaps = $inputText | Select-String '(.*) map:[\n\r]+((\d+ ?)*[\n\r]+)+' -AllMatches | Select -ExpandProperty Matches | ForEach-Object { [PSCustomObject]@{Name = $_.Groups[1].Value; Map = $_.Groups[2].Captures } }    
+# $maps = $rawMaps | ForEach-Object {
+#     $tempMapping = @{}
+#     $_.Map | ForEach-Object {
+#         $destStart, $sourceStart, $mapLen = $_.Value.Trim() -split ' '
+#         for ($j = 0; $j -lt $mapLen; $j++) {
+#             $tempMapping.Add([string]([long]$sourceStart + $j), [string]([long]$destStart + $j))
+#         }
+#     }
+#     $tempMapping
+# }
+
+# $locations = $seeds | ForEach-Object{
+#     $currVal = $_
+#     $maps | ForEach-Object{
+#         $map = $_
+#         $currVal = $map.ContainsKey("$currVal") ? $map["$currVal"] : $currVal
+#     }
+#     $currVal
+# }
+# $part1 = ($locations | measure -Minimum).Minimum
+# write-host "Part 1: $part1"
