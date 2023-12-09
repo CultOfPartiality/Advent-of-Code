b7cafdd33e83757501121bbb44137cbd2683b7d5
SGA-JasonWridgway <jason.wridgway@safegroup.com.au>
2023-12-09 17:12:05 +1100

    Easy day

---
+++ day9/part2.ps1
@@ -0,0 +1,22 @@
+#Get input
+#$inputSource = "$PSScriptRoot/example.txt"
+$inputSource = "$PSScriptRoot/input.txt"
+
+# =================== Parse Data =================== 
+$dataSets = Get-Content $inputSource | %{ ,[long[]] ($_ -split " ") }
+
+$dataSets | %{
+    $dataSet = ,$_
+    $i=0
+    while(($dataSet[$i]|group).Length -ne 1){
+        #$dataSet += ,[long[]]($dataSet[$i][1..($dataSet[$i].Length-1)])
+        $dataSet += ,[long[]](1..($dataSet[$i].Length-1) | %{$dataset[$i][$_] - $dataset[$i][$_-1]})
+        $i++
+    }
+    $result = 0
+    while($i -ge 0){
+        $result=$dataSet[$i][0] - $result
+        $i--
+    }
+    $result
+}|measure -Sum|select -ExpandProperty Sum
