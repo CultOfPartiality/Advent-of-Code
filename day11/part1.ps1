aa04ebee8b956b6d24f8380a632106212cb9a37d
CultOfPartiality <jason@wridgway.net>
2023-12-12 22:08:36 +1100

    Generaised part 1 to do part 2

--- day11/part1.ps1
+++ day11/part1.ps1
@@ -4,41 +4,39 @@ function part1 {
     #Standard Inputs
     param ($path)
 
-    $rawInput = Get-Content $path
-
-    #
-    # Expand Universe
-    #
-
-    #Expand Rows
-    $expandedRows = $rawInput|%{$_ -match "#" ? $_ : $_,$_}
-    $maxRow = $expandedRows.Count-1
-    $maxCol = $expandedRows[0].Length-1
-    #Expand columns.
-    #Scan colum to check for galexy
-    $toExtendCol = 0..$maxCol | %{
-        $colIndex = $_
-        $colData = $expandedRows | %{$_[$colIndex]} | Join-String 
-        $colData -notmatch "#"
-    }
-    #if column contains galexy, duplicate entry in that column for every row
-    $expandedCols = 0..$maxRow | %{
-        $row = $_
-        ,@(0..($toExtendCol.Length-1) | %{ $toExtendCol[$_] ? $expandedRows[$row][$_],$expandedRows[$row][$_] : $expandedRows[$row][$_]})
-    }
-    #Expand rows
-    $universe = $expandedCols
+    $universe = Get-Content $path
     $maxRow = $universe.Count-1
     $maxCol = $universe[0].Length-1
 
     #
+    # Check for universe expansion
+    #
+
+    #Check which rows need expanding
+    $expandRows = $universe|%{$_ -notmatch "#"}
+
+    #Expand columns.
+    #Scan colum to check for galexy
+    $expandCols = 0..$maxCol | %{
+        $colIndex = $_
+        $colData = $universe | %{$_[$colIndex]} | Join-String 
+        $colData -notmatch "#"
+    }
+
+    #
     # Find all galexies
     #
+
     $galexies = 0..$maxRow | %{
         $row = $_
         ($universe[$row] | Join-String | Select-String '#' -AllMatches).Matches | %{
-            if($_.Index -ne $null) { 
-                [PSCustomObject]@{x=$_.Index;y=$row}
+            if($_.Index -ne $null) {
+                #add expansions
+                $numOfRowExpansions = $expandRows[0..$row] | measure -sum | select -ExpandProperty Sum
+                $numOfColExpansions = $expandCols[0..$_.Index] | measure -sum | select -ExpandProperty Sum
+                $x = $_.Index + $numOfColExpansions * ($global:expansion-1)
+                $y = $row + $numOfRowExpansions * ($global:expansion-1)
+                [PSCustomObject]@{x=$x;y=$y}
             }
         }
     }
@@ -58,8 +56,14 @@ function part1 {
 
 }
 
+$expansion = 2
 Unit-Test ${function:part1} "$PSScriptRoot\testcases\Part1_374.txt" 374
 # Unit-Test ${function:part1} "$PSScriptRoot\testcases\test2_8.txt" 8
+$expansion = 10
+Unit-Test ${function:part1} "$PSScriptRoot\testcases\Part1_374.txt" 1030
+$expansion = 100
+Unit-Test ${function:part1} "$PSScriptRoot\testcases\Part1_374.txt" 8410
 
+$expansion = 1000000
 $res = part1 -path "$PSScriptRoot\input.txt"
 write-host "Part 1: $res" -ForegroundColor Magenta
