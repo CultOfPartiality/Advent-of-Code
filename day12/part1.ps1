18fcb96a6e7de464daa75551208e4a102d916b71
jason <jason@wridgway.net>
2023-12-13 23:18:40 +1100

    Not there yet... I might have to generate the possibilities programatically

--- day12/part1.ps1
+++ day12/part1.ps1
@@ -1,10 +1,23 @@
-$map,$clue = "?#?#?#?#?#?#?#? 11,3,1,6" -split " "
+$map,$clue = "?#.?..?.##.###? 2,1,2,3" -split " "
 $clues = $clue -split ","
 $spacesCount = $map.Length - ($clues|measure -Sum).Sum
-$minSpaces = $spaces - ($clues.count -1)
-$moveableSpaces = $spaces - $minSpaces
+$minSpaces = $spacesCount - ($clues.count -1)
+$moveableSpaces = $spacesCount - $minSpaces
 $spaces = 0..($clues.count+1) | %{1}
 $spaces[0] = 0
 $spaces[-1] = $moveableSpaces
 
 #plan is to interate over space options, generate all possible options that are valid with given map
+
+Write-Host "Clue: $clue"
+
+
+
+0..($map.Length-1) | %{
+	if($_ % 2){
+		'#' * [int]$clues[($_-1)/2]
+	}
+	else{
+		'.' * $spaces[$_/2]
+	}
+} | Join-String
