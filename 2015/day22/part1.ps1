7dd7ad4e2d444c91123dd8398648c97f8d82500f
SGA-JasonWridgway <jason.wridgway@safegroup.com.au>
2024-08-06 19:51:59 +1000

    Made a start

--- 2015/day22/part1.ps1
+++ 2015/day22/part1.ps1
@@ -4,13 +4,46 @@
 #function Solution {
 #    param ($Path)
 
-#The following line is for development
-$Path = "$PSScriptRoot/testcases/test1.txt"
 
-$data = get-content $Path
+<#
 
-<#WRITE CODE HERE, TEST, THEN PUT IN FUNCTION #>
-    
+Looks like another search of all possibe states... If running in a queue, can check all the current steps at the same time.
+
+As per that video, look for steps where we can prune the tree to reduce possible search paths (where we would die, make 
+no/negative progress, etc)
+
+Can also explore caching the state, so that if we reach a known (or worse) state after spending more mana, we can also prune
+#>
+
+$Path = "$PSScriptRoot/input.txt"
+
+class GameState {
+    [int]$SpentMana = 0
+
+    [int]$PlayerHealth = 50
+    [int]$CurrentMana = 500
+
+    [int]$Effect_ShieldTurnsRem = 0
+    [int]$Effect_PoisonTurnsRem = 0
+    [int]$Effect_RechargeTurnsRem = 0
+
+    [int]$BossHealth
+    [int]$BossDamage
+
+    [Object]Clone(){
+        return $this.MemberwiseClone()
+      }
+}
+
+$GameState_Start = New-Object GameState
+$GameState_Start.BossHealth, $GameState_Start.BossDamage = get-content $Path | %{ [int]($_ | Select-String "\d+").Matches.Value}
+
+
+
+
+
+
+
 #}
 
 #Unit-Test  ${function:Solution} "$PSScriptRoot/testcases/test1.txt" x
