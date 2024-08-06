7ecd929d3cb9ab8c4a1a51a64aeeb948a1dbf695
jason <jason@wridgway.net>
2024-08-06 23:34:08 +1000

    Priority queues are very good

--- 2015/day22/part1.ps1
+++ 2015/day22/part1.ps1
@@ -17,7 +17,17 @@ Can also explore caching the state, so that if we reach a known (or worse) state
 
 $Path = "$PSScriptRoot/input.txt"
 
+enum Actions {
+  Magic
+  Drain
+  Shield
+  Poison
+  Recharge
+}
+
 class GameState {
+    [bool]$Valid = $true
+
     [int]$SpentMana = 0
 
     [int]$PlayerHealth = 50
@@ -33,14 +43,133 @@ class GameState {
     [Object]Clone(){
         return $this.MemberwiseClone()
       }
+    UseMana($manacost){
+      $this.SpentMana += $manacost
+      $this.CurrentMana -= $manacost
+    }
+    
+    simulateEffects(){
+      $this.Effect_ShieldTurnsRem -= ($this.Effect_ShieldTurnsRem -gt 0) ? 1 : 0
+      
+      if($this.Effect_PoisonTurnsRem -gt 0){
+        $this.BossHealth -= 3
+        $this.Effect_PoisonTurnsRem--
+      }
+    
+      if($this.Effect_RechargeTurnsRem -gt 0){
+        $this.CurrentMana+=101
+        $this.Effect_RechargeTurnsRem--
+      }
+    }
+
+    simulateTurn([Actions] $action){
+      
+      #Efects at the start of the Player's turn
+      $this.simulateEffects()
+      
+      #Player action
+      switch ($action) {
+        ([Actions]::Magic)     {
+          $this.UseMana(53)
+          $this.BossHealth -= 4
+        }
+        ([Actions]::Drain)     {
+          $this.UseMana(73)
+          $this.BossHealth -= 2
+          $this.PlayerHealth += 2
+        }
+        ([Actions]::Shield)    {
+          if($this.Effect_ShieldTurnsRem -eq 0){
+            $this.UseMana(113)
+            $this.Effect_ShieldTurnsRem = 6
+          }
+          else {
+            $this.Valid = $false
+          }
+        }
+        ([Actions]::Poison)    {
+          if($this.Effect_PoisonTurnsRem -eq 0){
+            $this.UseMana(173)
+            $this.Effect_PoisonTurnsRem=6
+          }
+          else {
+            $this.Valid = $false
+          }
+        }
+        ([Actions]::Recharge)  {
+          if($this.Effect_RechargeTurnsRem -eq 0){
+            $this.UseMana(229)
+            $this.Effect_RechargeTurnsRem=5
+          }
+          else {
+            $this.Valid = $false
+          }
+        }
+      }
+      
+      #Efects at the start of the boss's turn
+      $this.simulateEffects()
+      
+      #Boss action
+      if($this.BossHealth -gt 0){
+        $this.PlayerHealth -= ($this.Effect_ShieldTurnsRem -gt 0) ? [Math]::Max(1,$this.BossDamage-7) : $this.BossDamage
+      }
+      
+      #Other Validity Checks
+      $this.Valid =  $this.Valid -and ($this.CurrentMana -gt 0) -and ($this.PlayerHealth -gt 0)
+    }
 }
 
+
 $GameState_Start = New-Object GameState
+
+
 $GameState_Start.BossHealth, $GameState_Start.BossDamage = get-content $Path | %{ [int]($_ | Select-String "\d+").Matches.Value}
 
+#Debug case
+# $GameState_Start.PlayerHealth = 10
+# $GameState_Start.CurrentMana = 250
+# $GameState_Start.BossDamage = 8
+# $GameState_Start.BossHealth = 13
+# $GameState_Start.simulateTurn([Actions]::Poison)
+# $GameState_Start
+# $GameState_Start.simulateTurn([Actions]::Magic)
+# $GameState_Start
+# exit
 
+# $GameState_Start.PlayerHealth = 10
+# $GameState_Start.CurrentMana = 250
+# $GameState_Start.BossDamage = 8
+# $GameState_Start.BossHealth = 14
+# $GameState_Start.simulateTurn([Actions]::Recharge)
+# $GameState_Start.simulateTurn([Actions]::Shield)
+# $GameState_Start.simulateTurn([Actions]::Drain)
+# $GameState_Start.simulateTurn([Actions]::Poison)
+# $GameState_Start.simulateTurn([Actions]::Magic)
+# exit
 
+$PossibleGames = New-Object "System.Collections.Generic.PriorityQueue[psobject,int]"
+$PossibleGames.Enqueue($GameState_Start,$GameState_Start.BossHealth)
 
+$LeastManaSpent = [int32]::MaxValue
+
+while ($PossibleGames.Count) {
+  $gamestate = $PossibleGames.Dequeue()
+  [actions].GetEnumNames() | %{[Actions]$_} | %{
+    $newGameState = $gamestate.Clone()
+    $newGameState.simulateTurn($_)
+    if($newGameState.Valid -and $newGameState.BossHealth -le 0){
+      if($newGameState.SpentMana -lt $LeastManaSpent){
+        $LeastManaSpent = $newGameState.SpentMana
+        Write-Host "New best score: $LeastManaSpent"
+      }
+    }
+    elseif( $newGameState.Valid -and $newGameState.SpentMana -lt $LeastManaSpent){
+      $PossibleGames.Enqueue($newGameState.Clone(),$newGameState.BossHealth)
+    }
+    
+  }
+}
 
 
 
