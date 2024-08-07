df982f03d61910abd8c901453f30c9ef226fad4e
jason <jason@wridgway.net>
2024-08-08 08:32:46 +1000

    Moved $Path above function start
    This way, it's one less thing that needs to be commented out when converting the function to a unit-testable function

--- NewTemplate.ps1
+++ NewTemplate.ps1
@@ -14,11 +14,13 @@ $template = @'
 . "$PSScriptRoot\..\..\Unit-Test.ps1"
 . "$PSScriptRoot\..\..\UsefulStuff.ps1"
 
+#The following line is for development
+$Path = "$PSScriptRoot/testcases/test1.txt"
+
+
 #function Solution {
 #    param ($Path)
 
-#The following line is for development
-$Path = "$PSScriptRoot/testcases/test1.txt"
 
 $data = get-content $Path
 
@@ -29,7 +31,6 @@ $data = get-content $Path
 #Unit-Test  ${function:Solution} "$PSScriptRoot/testcases/test1.txt" x
 #$result = Solution "$PSScriptRoot\input.txt"
 #Write-Host "Part 1: $result" -ForegroundColor Magenta
-
 '@
 
 Out-File -FilePath "./$foldername/part1.ps1" -InputObject $template
