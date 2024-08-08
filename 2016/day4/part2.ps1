bd33dbff016e7e0111e9cfbe9ca559069a88d2da
SGA-JasonWridgway <jason.wridgway@safegroup.com.au>
2024-08-08 17:14:45 +1000

    Completed day 4

--- 2016/day4/part2.ps1
+++ 2016/day4/part2.ps1
@@ -3,35 +3,48 @@
 
 #The following line is for development
 $Path = "$PSScriptRoot/testcases/test1.txt"
+$Path = "$PSScriptRoot/input.txt"
 
 # function Solution {
 # 	param ($Path)
 
 
-	$data = get-content $Path | % {
-		$matches = (Select-String -InputObject $_ -pattern "([\w-]+)-(\d+)\[(.+)\]").Matches.Groups
-		[PSCustomObject]@{
-			code	 = $Matches[1].Value
-			sector   = $Matches[2].Value
-			checksum = $Matches[3].Value
-		}
+$data = get-content $Path | % {
+	$matches = (Select-String -InputObject $_ -pattern "([\w-]+)-(\d+)\[(.+)\]").Matches.Groups
+	[PSCustomObject]@{
+		code     = $Matches[1].Value
+		shiftedCode = ""
+		sector   = $Matches[2].Value
+		checksum = $Matches[3].Value
 	}
+}
 
-	$validData = $data | % {
-		$groupedCode = ($_.code -replace "-", "").ToCharArray() | group-object | Sort-Object -Property @{Expression = "Count"; Descending = $true }, @{Expression = "Name"; Descending = $false }
-		$predictedChecksum = $groupedCode[0..4].name -join ""
-		if ($predictedChecksum -eq $_.checksum) {
-			$_
-		}
+$validData = $data | % {
+	$groupedCode = ($_.code -replace "-", "").ToCharArray() | group-object | Sort-Object -Property @{Expression = "Count"; Descending = $true }, @{Expression = "Name"; Descending = $false }
+	$predictedChecksum = $groupedCode[0..4].name -join ""
+	if ($predictedChecksum -eq $_.checksum) {
+		$_
 	}
+}
     
-	$validData | %{
-		$shiftCount = $_.sector
-		$_.code.ToCharArray() | %{
-			
+$validData | % {
+	$shiftCount = $_.sector
+	$shiftedCode = $_.code.ToCharArray() | % {
+		$value = [byte][char]$_
+		if ($_ -eq "-") {
+			$value = " "
 		}
+		else {
+			for ($i = 0; $i -lt $shiftCount; $i++) {
+				$value = $value -eq 122 ? 97 : $value + 1
+			}
+		}
+		[char]$value
 	}
+	$_.shiftedCode = $shiftedCode -join ""
+}
 
+$validData |? {$_.shiftedCode | select-string "North"}
 
 # }
 
