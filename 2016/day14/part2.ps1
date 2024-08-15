f17fd13499406e3c342f6896b91aa1b15db9d06c
SGA-JasonWridgway <jason.wridgway@safegroup.com.au>
2024-08-16 00:50:07 +1000

    Too slow... maybe main PC?

--- 2016/day14/part2.ps1
+++ 2016/day14/part2.ps1
@@ -11,6 +11,7 @@ function MD5_Alt {
     [System.BitConverter]::ToString($md5.ComputeHash($utf8.GetBytes($inStr))).ToLower() -replace "-",""
 }
 
+
 function Solution {
     param ($Path)
 
@@ -21,8 +22,10 @@ function Solution {
     $prev1000Hashes = @()
 
     while ($validKeys.Count -ne 64) {
-        # $newHash = (MD5_Alt ($salt + $index)).ToLower()
-        $newHash = ([System.BitConverter]::ToString($md5.ComputeHash($utf8.GetBytes(($salt + $index)))).ToLower().Replace("-",""))
+        $inStr = ($salt + $index)
+        # 0..2016 | % { $inStr = (MD5 $inStr).ToLower() }
+        0..2016 | % { $inStr = ([System.BitConverter]::ToString($md5.ComputeHash($utf8.GetBytes($inStr))).ToLower().Replace("-","")) }
+        $newHash = $inStr
         $index++
     
         if ($prev1000Hashes.Count -eq 1000) {
@@ -43,12 +46,15 @@ function Solution {
         else {
             $prev1000Hashes += $newHash
         }
+        if($index % 1000 -eq 0){
+            write-host "$index hashes checked, $($validKeys.Count) keys found so far"
+        }
     
     }
     $index - 1000 - 1
     
 }
-Unit-Test  ${function:Solution} "$PSScriptRoot/testcases/test1.txt" 22728
+Unit-Test  ${function:Solution} "$PSScriptRoot/testcases/test1.txt" 22859
 $measuredTime = measure-command { $result = Solution "$PSScriptRoot\input.txt" }
 Write-Host "Part 1: $result`nExecution took $($measuredTime.TotalSeconds)s" -ForegroundColor Magenta
 
