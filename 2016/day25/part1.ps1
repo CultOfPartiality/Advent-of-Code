813be76513e2a14d708a77c7edac1786f920091a
SGA-JasonWridgway <jason.wridgway@safegroup.com.au>
2024-08-20 23:13:59 +1000

    Day 25 Complete.
    Not sure if starting with 0 is a valid check, so didn't use it

--- 2016/day25/part1.ps1
+++ 2016/day25/part1.ps1
@@ -35,7 +35,7 @@ foreach ($regName in $registerNames) {
 }
 $aInitValue = 1
 $validStateFound = $false
-while(-not $validStateFound){
+while (-not $validStateFound) {
     # Reset the registes and state cache
     # Set a to the next best starting value
     # Run the simulation until either:
@@ -52,60 +52,103 @@ while(-not $validStateFound){
     }
     $registers["a"] = $aInitValue
 
-	while ($pointer -lt $instructions.Count) {
-		$instruction = $instructions[$pointer]
-		switch ($instruction.op) {
-			"cpy" {
-				$registers[$instruction.arg2] = $registers[$instruction.arg1]
-				$pointer++
-			}
-			"load" {
-				$registers[$instruction.arg2] = [int]$instruction.arg1
-				$pointer++
-			}
-			"inc" {
-				$registers[$instruction.arg1]++
-				$pointer++
-			}
-			"dec" {
-				$registers[$instruction.arg1]--
-				$pointer++
-			}
-			"jnz" {
+    :InstructionLoop while ($pointer -lt $instructions.Count) {
+        $instruction = $instructions[$pointer]
+        switch ($instruction.op) {
+            "cpy" {
+                $registers[$instruction.arg2] = $registers[$instruction.arg1]
+                $pointer++
+            }
+            "load" {
+                $registers[$instruction.arg2] = [int]$instruction.arg1
+                $pointer++
+            }
+            "inc" {
+                $registers[$instruction.arg1]++
+                $pointer++
+            }
+            "dec" {
+                $registers[$instruction.arg1]--
+                $pointer++
+            }
+            "jnz" {
                 $value = ($instruction.arg1 -in $registerNames) ? $registers[$instruction.arg1] : [int]$instruction.arg1
                 $jump = ($instruction.arg2 -in $registerNames) ? $registers[$instruction.arg2] : [int]$instruction.arg2
-				$pointer += ($value -ne 0) ? $jump : 1
-			}
+                $pointer += ($value -ne 0) ? $jump : 1
+            }
             "out" {
                 $outValue = ($instruction.arg1 -in $registerNames) ? $registers[$instruction.arg1] : [int]$instruction.arg1
-                if($outValue -notin (0,1)){
-                    break
+                # Write-host $outValue
+                if ($outValue -notin (0, 1)) {
+                    break InstructionLoop
                 }
-                if($outValue -eq $lastOutput){
-                    break
+                if ($outValue -eq $lastOutput) {
+                    break InstructionLoop
                 }
                 $lastOutput = $outValue
                 $outputCount++
                 $pointer++
             }
-		}
+        }
         # Generate a hash of the game state (register and pointer values), and see if we've seen it before
         $hash = "$pointer,$($registers.a),$($registers.b),$($registers.c),$($registers.d)".GetHashCode()
-        if($cache.ContainsKey($hash) -and $outputCount -ge 2 ){
+        if ($cache.ContainsKey($hash) -and $outputCount -ge 3 -and $instruction.op -eq "out" ) {
             # We're in a loop, and have seen at least 01 or 10, so we're on the money
             $validStateFound = $true
+            break
         }
-        else{
+        else {
             $cache[$hash] = 1
         }
-	}
+    }
     # If we made it out, then the sequence doesn't generate forever, so that's no good.
-    if(-not $validStateFound){$aInitValue++}
-    if($aInitValue % 10 -eq 0){
-        write-host "Up tp $aInitValue checked so far"
+    if (-not $validStateFound) { $aInitValue++ }
+    if ($aInitValue % 10 -eq 0) {
+        write-host "Up to $aInitValue checked so far"
     }
 }
-write-host "$aInitValue produces the desired pattern"
+write-host "$aInitValue produces the desired pattern: " -NoNewline
+
+$pointer = 0
+$outputCount = 0
+foreach ($regName in $registerNames) {
+    $registers[$regName] = 0
+}
+$registers["a"] = $aInitValue
+
+while ($pointer -lt $instructions.Count -and $outputCount -lt 40) {
+    $instruction = $instructions[$pointer]
+    switch ($instruction.op) {
+        "cpy" {
+            $registers[$instruction.arg2] = $registers[$instruction.arg1]
+            $pointer++
+        }
+        "load" {
+            $registers[$instruction.arg2] = [int]$instruction.arg1
+            $pointer++
+        }
+        "inc" {
+            $registers[$instruction.arg1]++
+            $pointer++
+        }
+        "dec" {
+            $registers[$instruction.arg1]--
+            $pointer++
+        }
+        "jnz" {
+            $value = ($instruction.arg1 -in $registerNames) ? $registers[$instruction.arg1] : [int]$instruction.arg1
+            $jump = ($instruction.arg2 -in $registerNames) ? $registers[$instruction.arg2] : [int]$instruction.arg2
+            $pointer += ($value -ne 0) ? $jump : 1
+        }
+        "out" {
+            $outValue = ($instruction.arg1 -in $registerNames) ? $registers[$instruction.arg1] : [int]$instruction.arg1
+            Write-host $outValue -NoNewline -ForegroundColor DarkGreen
+            $outputCount++
+            $pointer++
+        }
+    }
+}
+
 # }
 # Unit-Test  ${function:Solution} "$PSScriptRoot/testcases/test1.txt" 42
 # $measuredTime = measure-command { $result = Solution "$PSScriptRoot\input.txt" }
