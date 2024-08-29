5fc058f014d4878301fb1f86fe788b7c0a7da1ae
SGA-JasonWridgway <jason.wridgway@safegroup.com.au>
2024-08-29 19:52:15 +1000

    Day 14 complete

--- 2017/day14/part2.ps1
+++ 2017/day14/part2.ps1
@@ -9,21 +9,76 @@ $in = "flqrgnkx"
 
 function Solution {
 	param ($in)
-
+	Write-host "Generating hashes"
 	$hashes = 0..127 | % { Knot-Hash ($in + "-" + $_) }
 	$binaryData = $hashes | % {
-		$_.ToCharArray() | % {
-			$int = [System.Convert]::ToInt32($_, 16)
-			[system.convert]::ToString($int, 2).PadLeft(4, "0")
-		} | Join-String
+		, [int[]]($_.ToCharArray() | % {
+				$int = [System.Convert]::ToInt32($_, 16)
+				[system.convert]::ToString($int, 2).PadLeft(4, "0").ToCharArray() | % { [int][string]$_ }
+			})
 	}
 	
-	#Now do blob detection...
-	#todo
-	#todo
+	# Start with a random used square, and search outward using a queue
+	# Once used, modify the data to be blank
+	# Once all attached squares found, start with another set square
+	$groups = 0
+	$startRow = 0
+	$totalUnchecked = ($binaryData | % { $_ | ? { $_ -eq 1 } } | group | ? { $_.name -eq 1 } | select -ExpandProperty Count)
+	while ($totalUnchecked -gt 0) {
+		# Find new starting point
+		:findStartPoint for ($row = $startRow; $row -lt $binaryData.Count; $row++) {
+			if (($binaryData[$row] -eq 1).Count -eq 0) { $startRow++; continue }
+			for ($col = 0; $col -lt $binaryData[0].Count; $col++) {
+				if ($binaryData[$row][$col]) {
+					$startPoint = [PSCustomObject]@{
+						row = $row
+						col = $col
+					}
+					break findStartPoint
+				}
+			}
+		}
+
+		# Search and remove, using a queue
+		$searchSpace = New-Object System.Collections.Queue
+		$searchSpace.Enqueue($startPoint)
+		while ($searchSpace.Count) {
+			$point = $searchSpace.Dequeue()
+			if($binaryData[$point.row][$point.col] -eq 0){continue}
+			
+			$binaryData[$point.row][$point.col] = 0
+			$totalUnchecked--
+			
+			if (
+				($point.row -lt ($binaryData.Count-1)) -and 
+				($binaryData[$point.row + 1][$point.col] -eq 1)
+			){$searchSpace.Enqueue([PSCustomObject]@{row = $point.row + 1; col = $point.col })}
+			
+			if (
+				($point.row -gt 0) -and
+				($binaryData[$point.row - 1][$point.col] -eq 1)
+			) { $searchSpace.Enqueue([PSCustomObject]@{row = $point.row - 1; col = $point.col }) }
+			
+			if (
+				($point.col -lt ($binaryData[0].Count-1)) -and
+				($binaryData[$point.row][$point.col + 1] -eq 1)
+			) { $searchSpace.Enqueue([PSCustomObject]@{row = $point.row; col = $point.col + 1 }) }
+
+			if (
+				($point.col -gt 0) -and
+				($binaryData[$point.row][$point.col - 1] -eq 1)
+			){ $searchSpace.Enqueue([PSCustomObject]@{row = $point.row; col = $point.col - 1 }) }
+		}
+		$groups++
+		if ($groups % 10 -eq 0) {
+			write-host "$groups groups found so far..."
+		}
+	}
+
+	$groups
 }
 
-Unit-Test  ${function:Solution} "flqrgnkx" 8108
+Unit-Test  ${function:Solution} "flqrgnkx" 1242
 $measuredTime = measure-command { $result = Solution "hfdlxzhv" }
 Write-Host "Part 2: $result`nExecution took $($measuredTime.TotalSeconds)s" -ForegroundColor Magenta
 
