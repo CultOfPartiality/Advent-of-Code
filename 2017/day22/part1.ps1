8bf03a1ad51d81ae7c297c03f8ead5d6aaaf7ace
jason <jason@wridgway.net>
2024-09-18 22:37:14 +1000

    In progress (barely)

---
+++ 2017/day22/part1.ps1
@@ -0,0 +1,21 @@
+. "$PSScriptRoot\..\..\Unit-Test.ps1"
+. "$PSScriptRoot\..\..\UsefulStuff.ps1"
+
+#The following line is for development
+$Path = "$PSScriptRoot/testcases/test1.txt"
+
+#function Solution {
+#    param ($Path)
+
+
+$data = get-content $Path
+
+# --- The Plan ---
+# Do we have to handle an arbitary board size? Or can we get away with just a 'big' board?
+# No idiot, just keep a hashmap of all locations seen thus far, and track the location of the virus
+    
+#}
+#Unit-Test  ${function:Solution} "$PSScriptRoot/testcases/test1.txt" x
+#$measuredTime = measure-command {$result = Solution "$PSScriptRoot\input.txt"}
+#Write-Host "Part 1: $result`nExecution took $($measuredTime.TotalSeconds)s" -ForegroundColor Magenta
+
