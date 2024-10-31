90da82787c7db2594a91b462a2e0a00e490997fa
SGA-JasonWridgway <jason.wridgway@safegroup.com.au>
2024-10-31 19:19:35 +1100

    Day 7 complete. Could have been neater on part 2...

--- 2018/day7/part2.ps1
+++ 2018/day7/part2.ps1
@@ -2,15 +2,14 @@
 . "$PSScriptRoot\..\..\UsefulStuff.ps1"
 
 #The following line is for development
-$Path = "$PSScriptRoot/testcases/test1.txt"
+$inputData = @{Path="$PSScriptRoot/testcases/test1.txt"; WorkerCount=2; ExtraDelay=0}
 
 function Solution {
-    param ($Path)
-
+    param ($inputData)
 
     # Loop over data, building up a graph
     $steps = @{}
-    get-content $Path | % {
+    get-content $inputData.Path | % {
         $precursor = $_[5]
         $nextStep = $_[36]
         if (-not($steps.ContainsKey($precursor))) {
@@ -18,6 +17,7 @@ function Solution {
                 Name       = $precursor
                 NextSteps  = [System.Collections.ArrayList]@()
                 Precursors = [System.Collections.ArrayList]@()
+                WorkTime   = ( (1 + [int]$precursor - [int][char]"A") + $inputData.ExtraDelay)
             }
         }
         if (-not($steps.ContainsKey($nextStep))) {
@@ -25,6 +25,7 @@ function Solution {
                 Name       = $nextStep
                 NextSteps  = [System.Collections.ArrayList]@()
                 Precursors = [System.Collections.ArrayList]@()
+                WorkTime   = ( (1 + [int]$nextStep - [int][char]"A") + $inputData.ExtraDelay)
             }
         }
         $null = $steps[$precursor].NextSteps.Add($steps[$nextStep])
@@ -33,22 +34,47 @@ function Solution {
 
     $answer = ""
     $stillToGo = $steps.Values
-    while ($stillToGo.Count) {
-        # remove the first complete (sorted by name)
-        $complete = $stillToGo | ? { $_.precursors.count -eq 0 } | sort Name | select -First 1
-        $stillToGo = $stillToGo | ? { $_ -ne $complete }
-    
-        # Add to answer, then remove from precursors of remaining steps
-        # Repeat until all complete
-        $answer += $complete.Name
-        for ($i = 0; $i -lt $stillToGo.Count; $i++) {
-            $stillToGo[$i].precursors.Remove($complete)
+    $workers = 1..$inputData.WorkerCount | %{
+        $num = $_
+        [PSCustomObject]@{
+            Id = $num
+            WorkingOn = $null
         }
     }
-    $answer
+    $time = 0
+    while ( ($stillToGo.Count -gt 0) -or ( ($workers.WorkingOn -ne $null).Count -gt 0) ) {
+        $time++
+        
+        # remove the complete ones (sorted by name) and allocate to a worker if available
+        $nextTasks = $stillToGo | ? { $_.precursors.count -eq 0 } | sort Name
+        $nextTasks | %{
+            $task = $_
+            $availableWorkers = $workers | ?{$_.WorkingOn -eq $null}
+            if($availableWorkers.count -gt 0){
+                $availableWorkers[0].WorkingOn = $task
+                $stillToGo = $stillToGo | ? { $_ -ne $task }
+            }
+        }
+        
+    
+        # Run all workers
+        $workers | ?{$_.WorkingOn -ne $null} | sort {$_.WorkingOn.Name} |%{
+            $_.WorkingOn.WorkTime--
+
+            # If done, add to answer, then remove from precursors of remaining steps
+            if($_.WorkingOn.WorkTime -eq 0){
+                $answer += $_.WorkingOn.Name
+                for ($i = 0; $i -lt $stillToGo.Count; $i++) {
+                    $stillToGo[$i].precursors.Remove($_.WorkingOn)
+                }
+                $_.WorkingOn = $null
+            }
+        }
+    }
+    $time
     
 }
-Unit-Test  ${function:Solution} "$PSScriptRoot/testcases/test1.txt" "CABDFE"
-$measuredTime = measure-command { $result = Solution "$PSScriptRoot\input.txt" }
-Write-Host "Part 1: $result`nExecution took $($measuredTime.TotalSeconds)s" -ForegroundColor Magenta
+Unit-Test  ${function:Solution} @{Path="$PSScriptRoot/testcases/test1.txt"; WorkerCount=2; ExtraDelay=0} 15
+$measuredTime = measure-command { $result = Solution @{Path="$PSScriptRoot/input.txt"; WorkerCount=5; ExtraDelay=60} }
+Write-Host "Part 2: $result`nExecution took $($measuredTime.TotalSeconds)s" -ForegroundColor Magenta
 
