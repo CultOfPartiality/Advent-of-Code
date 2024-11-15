f91bbcd0251b8740b1982882f4ed198d0528c0db
CultOfPartiality <jason@wridgway.net>
2024-11-15 21:46:00 +1100

    Changes to int arrays, still took 180s

--- 2018/day9/part2.ps1
+++ 2018/day9/part2.ps1
@@ -14,21 +14,33 @@ function Solution {
     }
     $elves = @{}
     write-host "Play area populating..."
-    $playArea = 0..$setup.lastMarble | %{
-        $marble = $_
-        [PSCustomObject]@{
-            Marble = $marble
-            NextIndex = -1
-            PrevIndex = -1
-        }
-    } #new-object "psobject[]" ($setup.lastMarble+1)
-    $playArea[0].NextIndex = 2
-    $playArea[0].PrevIndex = 1
-    $playArea[1].NextIndex = 0
-    $playArea[1].PrevIndex = 2
-    $playArea[2].NextIndex = 1
-    $playArea[2].PrevIndex = 0
-    $currentNode = $playArea[2]
+    $playArea = New-Object "psobject[]" $setup.lastMarble
+    $nextIndexArray = New-Object "int[]" $setup.lastMarble
+    $prevIndexArray = New-Object "int[]" $setup.lastMarble
+    # 0..$setup.lastMarble | %{
+    #     $marble = $_
+    #     $playArea[$_] = 
+    #     [PSCustomObject]@{
+    #         Marble = $marble
+    #         NextIndex = -1
+    #         PrevIndex = -1
+    #     }
+    # } #new-object "psobject[]" ($setup.lastMarble+1)
+    # $playArea[0].NextIndex = 2
+    # $playArea[0].PrevIndex = 1
+    # $playArea[1].NextIndex = 0
+    # $playArea[1].PrevIndex = 2
+    # $playArea[2].NextIndex = 1
+    # $playArea[2].PrevIndex = 0
+    # $currentNode = $playArea[2]
+
+    $nextIndexArray[0] = 2
+    $prevIndexArray[0] = 1
+    $NextIndexArray[1] = 0
+    $PrevIndexArray[1] = 2
+    $NextIndexArray[2] = 1
+    $PrevIndexArray[2] = 0
+    $currentNodeIndex = 2
 
     write-host " -> Complete"
 
@@ -36,33 +48,30 @@ function Solution {
     foreach ($marble in 3..$setup.lastMarble) {
         if ($marble % 23 -eq 0) {
             #Remove the node 7 back
-            1..7 | %{$currentNode = $playArea[$currentNode.PrevIndex]}
-            $elves[$currentElf] += $marble + $currentNode.Marble
-            $preNode = $playArea[$currentNode.PrevIndex]
-            $postNode = $playArea[$currentNode.NextIndex]
-            $preNode.NextIndex = $postNode.Marble
-            $postNode.PrevIndex = $preNode.Marble
-            $currentNode = $postNode
+            1..7 | %{$currentNodeIndex = $prevIndexArray[$currentNodeIndex]}
+            $elves[$currentElf] += $marble + $currentNodeIndex
+            $preNodeIndex = $prevIndexArray[$currentNodeIndex]
+            $postNodeIndex = $nextIndexArray[$currentNodeIndex]
+            $NextIndexArray[$preNodeIndex] = $postNodeIndex
+            $prevIndexArray[$postNodeIndex] = $preNodeIndex
+            $currentNodeIndex = $postNodeIndex
         }
         else {
             #Insertion into the linked list
-            $preNode = $playArea[$currentNode.NextIndex]
-            $postNode = $playArea[$preNode.NextIndex]
+            $preNodeIndex = $nextIndexArray[$currentNodeIndex]
+            $postNodeIndex = $nextIndexArray[$preNodeIndex]
             #insert
-            $playArea[$marble] = [PSCustomObject]@{
-                Marble = $marble
-                NextIndex = $postNode.Marble
-                PrevIndex = $preNode.Marble
-            }
+            $nextIndexArray[$marble] = $postNodeIndex
+            $prevIndexArray[$marble] = $preNodeIndex
             #update previous entries
-            $preNode.NextIndex = $marble
-            $postNode.PrevIndex = $marble
-            $currentNode = $playArea[$marble]
+            $nextIndexArray[$preNodeIndex] = $marble
+            $prevIndexArray[$postNodeIndex] = $marble
+            $currentNodeIndex = $marble
         }
         $currentElf = ($currentElf + 1) % $setup.players
 
         #Progress
-        if($marble % 100000 -eq 0){
+        if($marble % 500000 -eq 0){
             $time = get-date
             write-host "Marble $marble, $((100.0*$marble/$setup.lastMarble).ToString("0.0"))%, last run took $($time.Subtract($currentTime).TotalSeconds)s"
             $currentTime = get-date
