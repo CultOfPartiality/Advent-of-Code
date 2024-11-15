929455967a847e8e68b608b6011339ac0ec76a66
CultOfPartiality <jason@wridgway.net>
2024-11-15 20:21:20 +1100

    Linked lists to the rescue. Still slow, but at least it finishes.
    Looks like preallocating the memory for all the objects is what takes up so much time... Maybe optimise?

--- 2018/day9/part2.ps1
+++ 2018/day9/part2.ps1
@@ -6,39 +6,79 @@ $Path = "$PSScriptRoot/testcases/test0.txt"
 
 function Solution {
     param ($Path)
-
-
     # Parse input
     $inputParams = (get-content $Path) -split " "
     $setup = [PSCustomObject]@{
         players    = ([int]$inputParams[0])
         lastMarble = (100*[int]$inputParams[6])
     }
-
-    # Setup elves and play area
-    $playArea = [System.Collections.ArrayList]@(0, 2, 1)
     $elves = @{}
-    $currentElf = 2 #starting from elf 0
-    $currentMarbleIndex = 1
+    write-host "Play area populating..."
+    $playArea = 0..$setup.lastMarble | %{
+        $marble = $_
+        [PSCustomObject]@{
+            Marble = $marble
+            NextIndex = -1
+            PrevIndex = -1
+        }
+    } #new-object "psobject[]" ($setup.lastMarble+1)
+    $playArea[0].NextIndex = 2
+    $playArea[0].PrevIndex = 1
+    $playArea[1].NextIndex = 0
+    $playArea[1].PrevIndex = 2
+    $playArea[2].NextIndex = 1
+    $playArea[2].PrevIndex = 0
+    $currentNode = $playArea[2]
 
+    write-host " -> Complete"
+
+    $currentTime = get-date
     foreach ($marble in 3..$setup.lastMarble) {
         if ($marble % 23 -eq 0) {
-            $currentMarbleIndex = ($currentMarbleIndex - 7 + $playArea.Count) % $playArea.Count
-            $elves[$currentElf] += $marble + $playArea[$currentMarbleIndex]
-            $playArea.RemoveAt($currentMarbleIndex)
+            #Remove the node 7 back
+            1..7 | %{$currentNode = $playArea[$currentNode.PrevIndex]}
+            $elves[$currentElf] += $marble + $currentNode.Marble
+            $preNode = $playArea[$currentNode.PrevIndex]
+            $postNode = $playArea[$currentNode.NextIndex]
+            $preNode.NextIndex = $postNode.Marble
+            $postNode.PrevIndex = $preNode.Marble
+            $currentNode = $postNode
         }
         else {
-            if (($currentMarbleIndex + 2) -eq ($playArea.Count) ) {
-                $currentMarbleIndex = $playArea.Add($marble)
+            #Insertion into the linked list
+            $preNode = $playArea[$currentNode.NextIndex]
+            $postNode = $playArea[$preNode.NextIndex]
+            #insert
+            $playArea[$marble] = [PSCustomObject]@{
+                Marble = $marble
+                NextIndex = $postNode.Marble
+                PrevIndex = $preNode.Marble
             }
-            else {
-                $currentMarbleIndex = ($currentMarbleIndex + 2) % ($playArea.Count)
-                $playArea.Insert($currentMarbleIndex, $marble)
-            }
+            #update previous entries
+            $preNode.NextIndex = $marble
+            $postNode.PrevIndex = $marble
+            $currentNode = $playArea[$marble]
         }
         $currentElf = ($currentElf + 1) % $setup.players
-        # write-host ($playArea -join ",")
+
+        #Progress
+        if($marble % 100000 -eq 0){
+            $time = get-date
+            write-host "Marble $marble, $((100.0*$marble/$setup.lastMarble).ToString("0.0"))%, last run took $($time.Subtract($currentTime).TotalSeconds)s"
+            $currentTime = get-date
+        }
+        
+        #debug
+    #     $debugNode = $playArea[0]
+    #     0..$marble | %{
+    #         write-host -NoNewline "$($debugNode.Marble),"
+    #         $debugNode = $playArea[$debugNode.NextIndex]
+    #     }
+    #     Write-Host
     }
+
+
+
 
     ($elves.Values | measure -Maximum).Maximum
 }
@@ -50,5 +90,6 @@ function Solution {
 # Unit-Test  ${function:Solution} "$PSScriptRoot/testcases/test4.txt" 54718
 # Unit-Test  ${function:Solution} "$PSScriptRoot/testcases/test5.txt" 37305
 $measuredTime = measure-command {$result = Solution "$PSScriptRoot\input.txt"}
+write-host "make sure to add back the *100"
 Write-Host "Part 2: $result`nExecution took $($measuredTime.TotalSeconds)s" -ForegroundColor Magenta
 
--- 2018/day9/part2_oldmethod.ps1
+++ 2018/day9/part2_oldmethod.ps1
@@ -12,7 +12,7 @@ function Solution {
     $inputParams = (get-content $Path) -split " "
     $setup = [PSCustomObject]@{
         players    = ([int]$inputParams[0])
-        lastMarble = ([int]$inputParams[6])
+        lastMarble = (100*[int]$inputParams[6])
     }
 
     # Setup elves and play area
@@ -21,6 +21,7 @@ function Solution {
     $elves = @{}
     $currentElf = 2 #starting from elf 0
     $currentMarbleIndex = 1
+    $currentTime = get-date
 
     foreach ($marble in 3..$setup.lastMarble) {
         if ($marble % 23 -eq 0) {
@@ -39,17 +40,22 @@ function Solution {
         }
         $currentElf = ($currentElf + 1) % $setup.players
         # write-host ($playArea -join ",")
+        if($marble % 100000 -eq 0){
+            $time = get-date
+            write-host "Marble $marble, $((100.0*$marble/$setup.lastMarble).ToString("0.0"))%, last run took $($time.Subtract($currentTime).TotalSeconds)s"
+            $currentTime = get-date
+        }
     }
 
     ($elves.Values | measure -Maximum).Maximum
 }
 
-Unit-Test  ${function:Solution} "$PSScriptRoot/testcases/test0.txt" 32
-Unit-Test  ${function:Solution} "$PSScriptRoot/testcases/test1.txt" 8317
-Unit-Test  ${function:Solution} "$PSScriptRoot/testcases/test2.txt" 146373
-Unit-Test  ${function:Solution} "$PSScriptRoot/testcases/test3.txt" 2764
-Unit-Test  ${function:Solution} "$PSScriptRoot/testcases/test4.txt" 54718
-Unit-Test  ${function:Solution} "$PSScriptRoot/testcases/test5.txt" 37305
+# Unit-Test  ${function:Solution} "$PSScriptRoot/testcases/test0.txt" 32
+# Unit-Test  ${function:Solution} "$PSScriptRoot/testcases/test1.txt" 8317
+# Unit-Test  ${function:Solution} "$PSScriptRoot/testcases/test2.txt" 146373
+# Unit-Test  ${function:Solution} "$PSScriptRoot/testcases/test3.txt" 2764
+# Unit-Test  ${function:Solution} "$PSScriptRoot/testcases/test4.txt" 54718
+# Unit-Test  ${function:Solution} "$PSScriptRoot/testcases/test5.txt" 37305
 $measuredTime = measure-command {$result = Solution "$PSScriptRoot\input.txt"}
-Write-Host "Part 1: $result`nExecution took $($measuredTime.TotalSeconds)s" -ForegroundColor Magenta
+Write-Host "Part 2 (x100): $result`nExecution took $($measuredTime.TotalSeconds)s" -ForegroundColor Magenta
 
