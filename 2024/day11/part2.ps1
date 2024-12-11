a254eda5009d1ee5f146b6fc34dacc508fed0422
CultOfPartiality <jason@wridgway.net>
2024-12-11 18:00:30 +1100

    Remove string operations

--- 2024/day11/part2.ps1
+++ 2024/day11/part2.ps1
@@ -20,9 +20,10 @@ function Solution {
             switch ($value) {
                 0 { $newStones[1] += $stones[$value] }
                 { $value -gt 9 -and [math]::Ceiling([math]::Log10($value+1)) % 2 -eq 0 } {
-                    $temp = [string]$value
-                    $val1 = [int]$temp.Substring(0,$temp.Length/2)
-                    $val2 = [int]$temp.Substring($temp.Length/2,$temp.Length/2)
+                    $numOfChars = [math]::Ceiling([math]::Log10($value+1))
+                    $splitter = [math]::Pow(10,($numOfChars/2))
+                    $val1 = [math]::Truncate($value / $splitter)
+                    $val2 = $value % $splitter
                     $newStones[$val1] += $stones[$value]
                     $newStones[$val2] += $stones[$value]
                 }
@@ -32,9 +33,10 @@ function Solution {
         $stones = $newStones
     }
 
+
     ($stones.Values | measure -sum).sum
     
 }
 $measuredTime = measure-command { $result = Solution "$PSScriptRoot\input.txt" }
-Write-Host "Part 1: $result`nExecution took $($measuredTime.TotalSeconds)s" -ForegroundColor Magenta
+Write-Host "Part 2: $result`nExecution took $($measuredTime.TotalSeconds)s" -ForegroundColor Magenta
 
