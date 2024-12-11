6aa946de9f91c103152e006080afdf2cb4b2fda4
jason <jason@wridgway.net>
2024-12-11 23:27:35 +1100

    Optimesed using priority queues for each gap size

--- 2024/day9/part2.ps1
+++ 2024/day9/part2.ps1
@@ -5,79 +5,85 @@
 $Path = "$PSScriptRoot/testcases/test1.txt"
 
 function Solution {
-    param ($Path)
+	param ($Path)
 
-    #TODO Nice documentation
+	# To optimise moving the files, we keep track of the gaps of each size in a priority queue
+    # Then for each move, check all the queues of at least that size for the first available 
+    # gap of the right size
 
-    $data = (get-content $Path).ToCharArray().ForEach({ [int]([string]$_) })
+	$data = (get-content $Path).ToCharArray().ForEach({ [int]([string]$_) })
 
-    $dataReg = $true
-    $index = 0
-    $id = 0
-    $files = [System.Collections.ArrayList]@()
-    $files.Capacity = [math]::Ceiling($data.Count / 2)
-    $gaps = [System.Collections.ArrayList]@()
-    $gaps.Capacity = [math]::Ceiling($data.Count / 2)
-    
-    foreach ($reg in $data) {
-        if ($dataReg) {
-            if ($reg -ne 0) {
-                [void]$files.add([PSCustomObject]@{
-                        id    = $id
-                        index = $index
-                        len   = $reg
-                    })
-            }
-            $id++
-        }
-        else {
-            if ($reg -ne 0) {
-                [void]$gaps.add([PSCustomObject]@{
-                        index = $index
-                        len   = $reg
-                    })
-            }
-        }
-        $index += $reg
-        $dataReg = -not $dataReg
-    }
+	$dataReg = $true
+	$index = 0
+	$id = 0
+	$files = [System.Collections.ArrayList]@()
+	$files.Capacity = [math]::Ceiling($data.Count / 2)
+	$gaps = 0..9 | % { , (new-object "System.Collections.Generic.PriorityQueue[psobject,int]") }
+  
+	foreach ($reg in $data) {
+		if ($dataReg) {
+			if ($reg -ne 0) {
+				[void]$files.add([PSCustomObject]@{
+						id    = $id
+						index = $index
+						len   = $reg
+					})
+			}
+			$id++
+		}
+		else {
+			if ($reg -ne 0) {
+				$gaps[$reg].Enqueue(
+					[PSCustomObject]@{
+						index = $index
+						len   = $reg
+					},$index)
+			}
+		}
+		$index += $reg
+		$dataReg = -not $dataReg
+	}
 
-    $gaps = [System.Collections.ArrayList]($gaps | sort { $_.index })
-    for ($i = ($files.Count - 1); $i -ge 0; $i--) {
-        $file = $files[$i]
-        # write-host "Working on file $($file.id)"
-        $oldFileIndex = $file.index
+	for ($i = ($files.Count - 1); $i -ge 0; $i--) {
+		$file = $files[$i]
 
-        # Get the next gap of adequate size
-        $gapindex = 0
-        while ( $gaps[$gapindex] -and $gaps[$gapindex].index -lt $file.index -and $gaps[$gapindex].len -lt $file.len ) { $gapindex++ } #Hotspot Still
-        $validGap = $gaps[$gapindex]
-        if ($validGap.index -gt $file.index -or $validGap.len -lt $file.len) { continue }
+		# Check the first gap in each bucket of adequate size and valid index, sorted by lowest index
+		$possibleGaps = foreach($gapLen in $file.len..9){
+			if($gaps[$gapLen].Count){
+				$gap = $gaps[$gapLen].Peek()
+				if($gap.index -lt $file.index){
+					$gap
+				}
+			}
+		}
+		if ($possibleGaps.count -lt 1) { continue }
+		$gap = ($possibleGaps | sort {$_.index})[0]
+		$validGap = $gaps[$gap.len].Dequeue()
 
-        $file.index = $validGap.index
-        #Make the gap smaller, or remove it if not required
-        if ($validGap.len -gt $file.len) {
-            $validGap.index += $file.len
-            $validGap.len = $validGap.len - $file.len
-        }
-        else {
-            $gaps.remove($validGap)
-        }
-    }
+		$file.index = $validGap.index
+		#Make the gap smaller, or "remove it" if not required
+		if ($validGap.len -gt $file.len) {
+			$validGap.index += $file.len
+			$validGap.len = $validGap.len - $file.len
+			$gaps[$validGap.len].Enqueue($validGap,$validGap.index)
+		}
+	}
 
-    # Calculate the hash and output
-    $total = 0
-    foreach ($file in $files) {
-        for ($i = 0; $i -lt $file.len; $i++) {
-            $total += $file.id * ($file.index + $i)
-        }
-    }
-    $total
+	# Calculate the hash and output
+	$total = 0
+	foreach ($file in $files) {
+		for ($i = 0; $i -lt $file.len; $i++) {
+			$total += $file.id * ($file.index + $i)
+		}
+	}
+	$total
 
 }
 Unit-Test  ${function:Solution} "$PSScriptRoot/testcases/test1.txt" 2858
-Unit-Test  ${function:Solution} "$PSScriptRoot/testcases/test2.txt" 1988987902
+Unit-Test  ${function:Solution} "$PSScriptRoot/testcases/test3.txt" 9
 Unit-Test  ${function:Solution} "$PSScriptRoot/testcases/test4.txt" 359
+Unit-Test  ${function:Solution} "$PSScriptRoot/testcases/test2.txt" 1988987902
+Unit-Test  ${function:Solution} "$PSScriptRoot/input.txt" 6469636832766
 $measuredTime = measure-command { $result = Solution "$PSScriptRoot\input.txt" }
 Write-Host "Part 2: $result`nExecution took $($measuredTime.TotalSeconds)s" -ForegroundColor Magenta
 
