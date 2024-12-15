361b7d8ed1cae143adf64eb90b388e98bb5bc89a
jason <jason@wridgway.net>
2024-12-15 23:50:14 +1100

    Still WIP

--- 2018/day16/part2.ps1
+++ 2018/day16/part2.ps1
@@ -8,7 +8,7 @@ $Path = "$PSScriptRoot/input.txt"
 # function Solution {
 # 	param ($Path)
 
-$instructions = 0..11 | % { , ("addr", "addi", "mulr", "muli", "banr", "bani", "borr", "bori", "setr", "seti", "gtir", "gtri", "gtrr", "eqir", "eqri", "eqrr") }
+$instructions = 0..15 | % { ,@() }
 
 $examplesRaw, $programRaw = ((get-content $Path -raw) -split "`r`n`r`n`r`n")
 $examples = $examplesRaw -split "`r`n" | Split-Array -GroupSize 4 
@@ -20,38 +20,53 @@ $examples | % {
     [int[]] $afterRegs = $_[2].TrimStart("After: [").TrimEnd("]") -split ", "
 
     $possibleOps = @()
-    if ($beforeRegs[$A] + $beforeRegs[$B] -eq $afterRegs[$C]) { $possibleOps += "addr" }
-    if ($beforeRegs[$A] + $B -eq $afterRegs[$C]) { $possibleOps += "addi" }
+    if ( ($beforeRegs[$A] + $beforeRegs[$B])     -eq $afterRegs[$C]) { $possibleOps += "addr" }
+    if ( ($beforeRegs[$A] + $B)                  -eq $afterRegs[$C]) { $possibleOps += "addi" }
 
-    if ($beforeRegs[$A] * $beforeRegs[$B] -eq $afterRegs[$C]) { $possibleOps += "mulr" }
-    if ($beforeRegs[$A] * $B -eq $afterRegs[$C]) { $possibleOps += "muli" }
+    if ( ($beforeRegs[$A] * $beforeRegs[$B])     -eq $afterRegs[$C]) { $possibleOps += "mulr" }
+    if ( ($beforeRegs[$A] * $B)                  -eq $afterRegs[$C]) { $possibleOps += "muli" }
 	
-    if (($beforeRegs[$A] -band $beforeRegs[$B]) -eq $afterRegs[$C]) { $possibleOps += "banr" }
-    if (($beforeRegs[$A] -band $B) -eq $afterRegs[$C]) { $possibleOps += "bani" }
+    if ( ($beforeRegs[$A] -band $beforeRegs[$B]) -eq $afterRegs[$C]) { $possibleOps += "banr" }
+    if ( ($beforeRegs[$A] -band $B)              -eq $afterRegs[$C]) { $possibleOps += "bani" }
 
-    if (($beforeRegs[$A] -bor $beforeRegs[$B]) -eq $afterRegs[$C]) { $possibleOps += "borr" }
-    if (($beforeRegs[$A] -bor $B) -eq $afterRegs[$C]) { $possibleOps += "bori" }
+    if ( ($beforeRegs[$A] -bor $beforeRegs[$B])  -eq $afterRegs[$C]) { $possibleOps += "borr" }
+    if ( ($beforeRegs[$A] -bor $B)               -eq $afterRegs[$C]) { $possibleOps += "bori" }
 
-    if ($beforeRegs[$A] -eq $afterRegs[$C]) { $possibleOps += "setr" }
-    if ($A -eq $afterRegs[$C]) { $possibleOps += "seti" }
+    if ( $beforeRegs[$A]                         -eq $afterRegs[$C]) { $possibleOps += "setr" }
+    if ( $A                                      -eq $afterRegs[$C]) { $possibleOps += "seti" }
 
     if ($afterRegs[$C] -in 0, 1) {
-        if (([int]($A -gt $beforeRegs[$B])) -eq $afterRegs[$C]) { $possibleOps += "gtir" }
-        if (([int]($beforeRegs[$A] -gt $B)) -eq $afterRegs[$C]) { $possibleOps += "gtri" }
+        if (([int]($A -gt $beforeRegs[$B]))      -eq $afterRegs[$C]) { $possibleOps += "gtir" }
+        if (([int]($beforeRegs[$A] -gt $B))      -eq $afterRegs[$C]) { $possibleOps += "gtri" }
         if (([int]($beforeRegs[$A] -gt $beforeRegs[$B])) -eq $afterRegs[$C]) { $possibleOps += "gtrr" }
 		
-        if (([int]($A -gt $beforeRegs[$B])) -eq $afterRegs[$C]) { $possibleOps += "eqir" }
-        if (([int]($beforeRegs[$A] -gt $B)) -eq $afterRegs[$C]) { $possibleOps += "eqri" }
+        if (([int]($A -gt $beforeRegs[$B]))      -eq $afterRegs[$C]) { $possibleOps += "eqir" }
+        if (([int]($beforeRegs[$A] -gt $B))      -eq $afterRegs[$C]) { $possibleOps += "eqri" }
         if (([int]($beforeRegs[$A] -gt $beforeRegs[$B])) -eq $afterRegs[$C]) { $possibleOps += "eqrr" }
-
     }
-    if ($instructions[$op].Count -gt 1) {
+
+    if ($instructions[$op].Count -eq 0) {
+        $instructions[$op] = $possibleOps
+    }
+    else{
         $instructions[$op] = $instructions[$op] | ? { $_ -in $possibleOps }
     }
 }
 
 # Worked out some instructions, now need to remove those from other possibilities and then run the program
+$z=0;$instructions | %{write-host ("$z - " + ($_ -join ","));$z++}
+while( ($instructions | %{$_.count}) -gt 1 ){
+    $foundInsts = $instructions | ?{$_.count -eq 1} 
+    for ($i = 0; $i -lt $instructions.Count; $i++) {
+        if($instructions[$i].count -gt 1){
+            $instructions[$i] = $instructions[$i] | ?{$_ -notin $foundInsts}
+        }
+    }
+    write-host "$(($instructions | %{$_}).Count) possibiilites"
+    $z=0;$instructions | %{write-host ("$z - " + ($_ -join ","));$z++}
+    write-host    
 
+}
 
 $z
 # }
