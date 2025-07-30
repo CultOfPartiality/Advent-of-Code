f960af6145d5c8f066270df3dc286f497451f4c9
CultOfPartiality <jason@wridgway.net>
2025-07-30 18:30:41 +1000

    Test array chunking against existing solution?

--- StuffToInvestigate.ps1
+++ StuffToInvestigate.ps1
@@ -2,3 +2,12 @@
 [Uint16]::PopCount(15)
 [Uint16]::RotateLeft(0x8000,1)
 [Uint16]::RotateRight(0x0001,1)
+
+
+#Splitting arrays (optimise some previous helper functions?)
+$bigList = 1..1000
+
+$counter = [pscustomobject] @{ Value = 0 }
+$groupSize = 100
+
+$groups = $bigList | Group-Object -Property { [math]::Floor($counter.Value++ / $groupSize) }
