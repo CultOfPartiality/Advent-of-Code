1f1bc31db9bacbec7705382a00b3d2f4c97462e7
CultOfPartiality <Jason@Wridgway.Net>
2025-09-06 14:23:28 +1000

    Part 1 done but slow

---
+++ 2019/day3/part1.ps1
@@ -0,0 +1,42 @@
+. "$PSScriptRoot\..\..\Unit-Test.ps1"
+. "$PSScriptRoot\..\..\UsefulStuff.ps1"
+
+#The following line is for development
+$Path = "$PSScriptRoot/testcases/test1.txt"
+
+function Solution {
+    param ($Path)
+
+    $wires = get-content $Path | % { , ($_ -split ",") }
+
+    $Steps = @{}
+    $bit = 1 #Each round sets a bit, to avoid counting self intersections
+    foreach ($wire in $wires) {
+        $coords = 0, 0 # Y,X, +ve is up and to the right
+        foreach ($step in $wire) {
+            for ($len = [int]$step.Substring(1); $len -gt 0; $len--) {
+                switch ($step[0]) {
+                    "U" { $coords[0]++ }
+                    "D" { $coords[0]-- }
+                    "R" { $coords[1]++ }
+                    "L" { $coords[1]-- }
+                }
+                $hash = "$($coords[0]),$($coords[1])"
+                $Steps[$hash] = $Steps[$hash] -bor $bit
+            }
+        }
+        $bit *= 2
+    }
+
+    $Intersections = $Steps.GetEnumerator() | ?{ $_.Value -eq 3 } | select -ExpandProperty Key
+    $Distances = $Intersections | % { [int[]]($_ -split ",") | Manhattan-Distance } | Sort-Object -Descending
+    $Distances[-1]
+
+
+}
+Unit-Test  ${function:Solution} "$PSScriptRoot/testcases/test1.txt" 6
+Unit-Test  ${function:Solution} "$PSScriptRoot/testcases/test2.txt" 159
+Unit-Test  ${function:Solution} "$PSScriptRoot/testcases/test3.txt" 135
+$measuredTime = measure-command {$result = Solution "$PSScriptRoot\input.txt"}
+Write-Host "Part 1: $result`nExecution took $($measuredTime.TotalSeconds)s" -ForegroundColor Magenta
+
