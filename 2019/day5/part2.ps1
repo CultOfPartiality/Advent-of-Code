2b40cb2662435ef98ab526c4c2c31cde5bb0b205
CultOfPartiality <Jason@Wridgway.Net>
2025-09-09 21:26:06 +1000

    2019 Day 5 Complete - Pwsh

---
+++ 2019/day5/part2.ps1
@@ -0,0 +1,82 @@
+. "$PSScriptRoot\..\..\Unit-Test.ps1"
+. "$PSScriptRoot\..\..\UsefulStuff.ps1"
+
+#The following line is for development
+# $Path = "$PSScriptRoot/testcases/test1.txt"
+$Path = "$PSScriptRoot/input.txt"
+
+function Solution {
+    param ($Path)
+
+    $data = (get-content $Path) -split ',' | % { [int]$_ } 
+
+    $index = 0
+    while ($data[$index] % 100 -ne 99) {
+        $indexes = $data[($index + 1)..($index + 3)]
+        $op = $data[$index] % 100
+    
+        $param1Mode = [Math]::Floor($data[$index] / 100) % 10
+        $param2Mode = [Math]::Floor($data[$index] / 1000) % 10
+        $param3Mode = [Math]::Floor($data[$index] / 10000) % 10
+    
+        $param1 = $param1Mode ? $indexes[0] : $data[$indexes[0]]
+        $param2 = $param2Mode ? $indexes[1] : $data[$indexes[1]]
+        $param3 = $param3Mode ? $indexes[2] : $data[$indexes[2]]
+
+        if ($op -eq 1) {
+            $data[$indexes[2]] = $param1 + $param2
+            $index += 4
+        }
+        elseif ($op -eq 2) {
+            $data[$indexes[2]] = $param1 * $param2
+            $index += 4
+        }
+        elseif ($op -eq 3) {
+            $data[$indexes[0]] = 5 #Only input gets a 5
+            $index += 2   
+        }
+        elseif ($op -eq 4) {
+            if ($data[$index + 2] -eq 99 ) {
+                $param1 # output
+                return
+            }
+            elseif ($param1 -ne 0) {
+                write-host "Failed at output index $index"
+                exit
+            }
+            $index += 2   
+        }
+        elseif ($op -eq 5) {
+            #Jump if true
+            $index+=3
+            if($param1 -ne 0){
+                $index = $param2
+            }
+        }
+        elseif ($op -eq 6) {
+            #Jump if false
+            $index+=3
+            if($param1 -eq 0){
+                $index = $param2
+            }
+        }
+        elseif ($op -eq 7) {
+            # Less than
+            $data[$indexes[2]] = [int]($param1 -lt $param2)
+            $index+=4
+        }
+        elseif ($op -eq 8) {
+            # Equals
+            $data[$indexes[2]] = [int]($param1 -eq $param2)
+            $index+=4
+        }
+        else {
+            write-host "Error, op code $op at index $index not supported" -ForegroundColor Red
+            exit
+        }
+    }
+}
+# Unit-Test  ${function:Solution} "$PSScriptRoot/../testcases/test1.txt" 3500
+$measuredTime = measure-command { $result = Solution "$PSScriptRoot/input.txt" }
+Write-Host "Part 2: $result`nExecution took $($measuredTime.TotalSeconds)s" -ForegroundColor Magenta
+
