8d39e2ddd8ba20f698a3da6015f414606ecc8c61
CultOfPartiality <Jason@Wridgway.Net>
2025-09-10 20:51:47 +1000

    2019 Day 6 Complete - Pwsh

---
+++ 2019/day6/part1.ps1
@@ -0,0 +1,33 @@
+. "$PSScriptRoot\..\..\Unit-Test.ps1"
+. "$PSScriptRoot\..\..\UsefulStuff.ps1"
+
+#The following line is for development
+$Path = "$PSScriptRoot/testcases/test1.txt"
+
+function Solution {
+    param ($Path)
+
+    $planets = @{}
+    get-content $Path | % {
+        $planet, $moon = $_ -split "\)"
+        $planets[$planet] += @($moon)
+    }
+
+    $searchSpace = New-Object 'System.Collections.Generic.PriorityQueue[psobject,int32]'
+    $totalOrbits = 0
+    $searchSpace.Enqueue('COM', 0)
+    while ($searchSpace.Count) {
+        [void]$searchSpace.TryDequeue([ref]$planet, [ref]$orbits)
+        $orbiters = $planets[$planet]
+        $orbits += 1
+        foreach ($orbiter in $orbiters) {
+            $totalOrbits += $orbits
+            $searchSpace.Enqueue($orbiter, $orbits)
+        }
+    }
+    $totalOrbits
+}
+Unit-Test  ${function:Solution} "$PSScriptRoot/testcases/test1.txt" 42
+$measuredTime = measure-command { $result = Solution "$PSScriptRoot\input.txt" }
+Write-Host "Part 1: $result`nExecution took $($measuredTime.TotalSeconds)s" -ForegroundColor Magenta
+
---
+++ 2019/day6/part2.ps1
@@ -0,0 +1,49 @@
+. "$PSScriptRoot\..\..\Unit-Test.ps1"
+. "$PSScriptRoot\..\..\UsefulStuff.ps1"
+
+#The following line is for development
+$Path = "$PSScriptRoot/testcases/test2.txt"
+
+function Solution {
+    param ($Path)
+
+    $planets = @{}
+    get-content $Path | % {
+        $planet, $moon = $_ -split "\)"
+        foreach ($body in $planet, $moon) {
+            if (!$planets.ContainsKey($body)) {
+                $planets[$body] = [PSCustomObject]@{
+                    Moons           = @()
+                    Orbits          = ''
+                    DistanceFromCOM = 0
+                }
+            }
+        }
+        $planets[$planet].Moons += @($moon)
+        $planets[$moon].Orbits = $planet
+    }
+
+    $searchSpace = New-Object 'System.Collections.Generic.PriorityQueue[psobject,int32]'
+    $searchSpace.Enqueue('COM', 0)
+    while ($searchSpace.Count) {
+        [void]$searchSpace.TryDequeue([ref]$planet, [ref]$orbits)
+        foreach ($moon in $planets[$planet].Moons) {
+            $planets[$moon].DistanceFromCOM = $orbits + 1
+            $searchSpace.Enqueue($moon, $orbits + 1)
+        }
+    }
+
+    $transfers = 0
+    while ($planets['YOU'].Orbits -ne $planets['SAN'].Orbits) {
+        $furthest = $planets['YOU'].DistanceFromCOM -gt $planets['SAN'].DistanceFromCOM ? 'YOU' : 'SAN'
+        $planets[$furthest].DistanceFromCOM--
+        $planets[$furthest].Orbits = $planets[$planets[$furthest].Orbits].Orbits
+        $transfers++
+    }
+    $transfers
+
+}
+Unit-Test  ${function:Solution} "$PSScriptRoot/testcases/test2.txt" 4
+$measuredTime = measure-command { $result = Solution "$PSScriptRoot\input.txt" }
+Write-Host "Part 2: $result`nExecution took $($measuredTime.TotalSeconds)s" -ForegroundColor Magenta
+
