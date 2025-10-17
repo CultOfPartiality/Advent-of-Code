2b9b27150e386cd1b4504c3de30b94e1c46d02be
CultOfPartiality <jason@wridgway.net>
2025-10-17 18:28:16 +1100

    2019 day 2 - Powershell

---
+++ 2019/day14/part2.ps1
@@ -0,0 +1,102 @@
+. "$PSScriptRoot\..\..\Unit-Test.ps1"
+. "$PSScriptRoot\..\..\UsefulStuff.ps1"
+
+#The following line is for development
+$Path = "$PSScriptRoot/testcases/test1.txt"
+
+function Solution {
+   param ($Path)
+
+    # Parse the data into recipies
+    $recipies = @{}
+    get-content $Path | %{
+        $rawIngredients,$rawResult = $_ -split " => "
+        $ingredients = $rawIngredients -split ", " | %{
+            [int]$num,$material = $_ -split " "
+            [PSCustomObject]@{
+                Material = $material
+                Amount = $num
+            }
+        }
+        [int]$num,$material = $rawResult -split " "
+        $recipies[$material] = [PSCustomObject]@{
+            Material = $material
+            Amount = $num
+            Ingredients = $ingredients
+            DistFromFuel = 0
+        }
+    }
+
+
+    # Charaterise the recipies in terms of the max distance to Fuel, then walk them 
+    # starting closest to fuel out, adding up how much of each is needed along the way.
+    function Walk-Recipies {
+        param(
+            $recipie,
+            $possibleDist
+        )
+        if($recipie.Ingredients.Material -ne "ORE"){
+            foreach($Ingredient in $recipie.Ingredients.Where({$_.Material -ne "ORE"})){
+                $subRecipie = $recipies[$Ingredient.Material]
+                Walk-Recipies $subRecipie ($possibleDist+1)
+            }
+        }
+        $recipie.DistFromFuel = [Math]::Max($recipie.DistFromFuel,$possibleDist)
+    }
+    Walk-Recipies $recipies["FUEL"] 0
+    
+    # Start at "FUEL", and work out from there
+    $NeedToMake = @{}
+    $NeedToMake["FUEL"] = [PSCustomObject]@{
+        Material = "FUEL"
+        Recipie = $recipies["FUEL"]
+        AmountRequired = 1
+    }
+    
+    $totalOre = 0
+    while($NeedToMake.Count){
+        $Make = $NeedToMake.Values | Sort-Object {$_.Recipie.DistFromFuel} | select -First 1
+        $NeedToMake.Remove($Make.Material)
+        
+        # For part 2, we just perform all calcs using floats, and round down at the end
+        $ActualBatchesToMake = $Make.AmountRequired/$Make.Recipie.Amount
+        $ActualAmountToMake = $Make.AmountRequired/$Make.Recipie.Amount*$Make.Recipie.Amount
+
+        write-host "To make $($Make.AmountRequired) lots of $($Make.Material), we'll have to make $ActualAmountToMake lots, which is $ActualBatchesToMake batches of $($Make.Recipie.Amount)x$($Make.Material):"
+        # $Make.Recipie
+        foreach($ingredient in $Make.Recipie.Ingredients){
+            $ingredientRecipie = $Recipies[$ingredient.Material]
+            $required = $ActualBatchesToMake*$ingredient.Amount
+            Write-Host "`t$required x $($ingredient.Material)"
+            if($ingredient.Material -eq "ORE"){
+                # $required = [MATH]::Ceiling($ActualAmountToMake/$Make.Recipie.Amount) * $ingredient.Amount
+                # Write-Host ("`t`t($($ActualBatchesToMake) batches is ($($Ingredient.Material)x$required)")
+                # $totalOre += [MATH]::Ceiling($required/$ingredientRecipie.Amount) * $ingredient.Amount
+                $totalOre += $required
+                continue
+            }
+            # Write-Host "`t`t(Each recipie for $($ingredient.Material) makes $($ingredientRecipie.Amount))"
+            # Write-Host "`t`t(We need to run the recipie $recipieRuns times, making $($recipieRuns* $ingredientRecipie.Amount) lots of $($ingredient.Material))"
+            if($NeedToMake.ContainsKey($ingredient.Material)){
+                $NeedToMake[$ingredient.Material].AmountRequired += $required
+            }
+            else{
+                $NeedToMake[$ingredient.Material] = [PSCustomObject]@{
+                    Material = $ingredient.Material
+                    Recipie = $recipies[$ingredient.Material]
+                    AmountRequired = $required
+                }
+            }
+        }    
+    }
+
+    # For part 2, we just perform all calcs using floats, and round down at the end
+    [Math]::Floor(1000000000000/$totalOre)
+    
+}
+Unit-Test  ${function:Solution} "$PSScriptRoot/testcases/test3.txt" 82892753
+Unit-Test  ${function:Solution} "$PSScriptRoot/testcases/test4.txt" 5586022
+Unit-Test  ${function:Solution} "$PSScriptRoot/testcases/test5.txt" 460664
+$measuredTime = measure-command {$result = Solution "$PSScriptRoot\input.txt"}
+Write-Host "Part 2: $result`nExecution took $($measuredTime.TotalSeconds)s" -ForegroundColor Magenta
+
