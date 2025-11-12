54a3f6d8a54e20400ca5388f9677b78cf79ee78e
CultOfPartiality <jason@wridgway.net>
2025-11-12 20:37:00 +1100

    Examples passed, but still too slow for real input...

--- 2019/day16/part2.ps1
+++ 2019/day16/part2.ps1
@@ -7,74 +7,32 @@ $Path = "$PSScriptRoot/testcases/test1.txt"
 function Solution {
     param ($Path)
 
-    $InputSignal = (get-content $Path).ToCharArray() | % { [int]"$_" }
-    $RealSignal = $InputSignal
+    $RealSignal = (get-content $Path).ToCharArray() | % { [int]"$_" }
+    $InputSignal_SecondHalf = $RealSignal*5000
+    $MessageOffset = [int]($RealSignal[0..6] -join "")
+    $MessageOffset_FromTheEnd = ($RealSignal.Count * 10000) - $MessageOffset
+    write-host "`nInput is $($RealSignal.Count) digits long"
+    write-host "Expanded input is $($RealSignal.Count * 10000) digits long"
+    write-host "Message offset is $MessageOffset"
 
-    $MessageOffset = [int]($InputSignal[0..6] -join "")
-
-    #This doesn't work. It might for the first round, but we don't know the next round will be the same pattern of duplicates input signals..
-    function FFT_NotIT {
-        param ($InputSignal)
-        $BasePattern = 0, 1, 0, -1
+    function FFT {
+        param ($InputSignal_SecondHalf,$MessageOffset)
         $result = @()
-        $InputSignalLong = $InputSignal  * 10000
-
-        # For each digit in the input signal...
-        for ($i = 0; $i -lt $InputSignalLong.Count; $i++) {
-            $Pattern = $BasePattern | % { @($_) * ($i + 1) }
-            
-            # Work out how many rounds of the signal are required for the pattern to line up
-            # e.g. for a length of 650, for the first digit (pattern length of 4) we'll need to do 2 rounds.    Then we can just multiply that by 10000/2 = 5000
-            #                           for the second digit (pattern length of 8) we'll need to do 4 rounds.   Then we can just multiply that by 10000/4 = 2500
-            #                           for the third digit (pattern length of 12) we'll need to do 6 rounds.   Then we can just multiply that by 10000/6 = 1666.666.....?
-            #                           for the fourth digit (pattern length of 16) we'll need to do 8 rounds.  Then we can just multiply that by 10000/8 = 1250
-            #                           for the fifth digit (pattern length of 20) we'll need to do 2 rounds.   Then we can just multiply that by 10000/2 = 5000
-            #       Digit | Pattern Len | Rounds to whole num | Multiply
-            #       ------+-------------+---------------------+----------------
-            #       0     | 4           | 2                   | 10000/2  = 5000
-            #       1     | 8           | 4                   | 10000/4  = 2500
-            #       2     | 12          | 6                   | 10000/6  = 1666, then do (10000 - 1666*6) = 4 repeats of the input signal more
-            #       3     | 16          | 8                   | 10000/8  = 1250
-            #       4     | 20          | 2                   | 10000/2  = 5000
-            #       5     | 24          | 12                  | 10000/12 = 833, then do 4 repeats of the input signal more
-            #       6     | 28          | 14                  | 10000/14 = 714, then do 4 repeats of the input signal more
-            #       7     | 32          | 16                  | 10000/16 = 625
-            
-
-            $RoundsOfInput = (lcm $InputSignal.Count $Pattern.Count)/$InputSignal.Count
-            $MultiplyFactorTemp = [Math]::DivRem(10000,$RoundsOfInput)
-            $Multiply = $MultiplyFactorTemp.Item1
-            $ExtraInputSignalsAfter = $MultiplyFactorTemp.Item2
-            # write-host "Digit $i | Pattern length $($Pattern.Count) | Rounds of Input Signal that line up with pattern: $RoundsOfInput | Multiply: $Multiply | Extra Input Signals: $ExtraInputSignalsAfter"
-            
-            #Do the initial round of input signals
-            $InputSignalDuplicated = $InputSignal * $RoundsOfInput
-            $PatternIndex = $i
-            $tot = 0
-            for ($j = 0; $j -lt $InputSignalDuplicated.Count; $j++) {
-                $PatternIndex = ($PatternIndex + 1) % $Pattern.Count
-                $tot += $InputSignalDuplicated[$j] * $Pattern[$PatternIndex]
-            }
-            $tot = ([Math]::ABS($tot % 10) * $Multiply) % 10
-            
-            #Extra rounds
-            $InputSignalExtra = $InputSignal * $ExtraInputSignalsAfter
-            $PatternIndex = $i
-            $tot = 0
-            for ($j = 0; $j -lt $InputSignalExtra.Count; $j++) {
-                $PatternIndex = ($PatternIndex + 1) % $Pattern.Count
-                $tot += $InputSignalExtra[$j] * $Pattern[$PatternIndex]
-            }
-            $tot = ([Math]::ABS($tot % 10) * $Multiply) % 10
+        $tot = 0
+        for ($i = 0; $i -lt $MessageOffset; $i++) {
+            $tot = ($tot + $InputSignal_SecondHalf[0-1-$i]) % 10
             $result += $tot
         }
+        [array]::Reverse($result)
         $result
     }
 
-    $signal = $RealSignal
-    1..100 | % { $signal = FFT -InputSignal $signal }
+    1..100 | % {
+        $InputSignal_SecondHalf = FFT -InputSignal $InputSignal_SecondHalf -MessageOffset $MessageOffset_FromTheEnd
+        write-host "Completed iteration $_"
+    }
 
-    [int]($signal[$MessageOffset..($MessageOffset+7)] -join "")
+    [int]($InputSignal_SecondHalf[0..7] -join "")
 
 }
 Unit-Test  ${function:Solution} "$PSScriptRoot/testcases/test4.txt" 84462026
