5c351117cca1018b971532d0daa04c6e23a60d88
CultOfPartiality <jason@wridgway.net>
2025-11-14 20:17:15 +1100

    Almost there (by hand at least), just need to pass back into intcomp

--- 2019/day17/part2.ps1
+++ 2019/day17/part2.ps1
@@ -9,8 +9,8 @@ $Path = "$PSScriptRoot/input.txt"
 
 # function Solution {
 #     param ($Path)
-
-$ASCII = [Computer]::New(((get-content $Path) -split ',' | % { [int64]$_ }))
+$memory = (get-content $Path) -split ',' | % { [int64]$_ }
+$ASCII = [Computer]::New($memory)
 
 while (!$ASCII.complete) {
     $ASCII.RunComputer($null)
@@ -19,20 +19,21 @@ while (!$ASCII.complete) {
 # Write map
 # $ASCII.outputBuffer | % { [char]$_ } | Join-String -Separator "" | write-host
 
+
+# Setup the map and robot
 $robot = [PSCustomObject]@{
     dir = 0 # 0-3: Up,Right,Down,Left
     x   = 0
     y   = 0
 }
-$initialDir = @{"^" = 0; ">" = 1; "v" = 2; "<" = 3 }
-$dirDiffToTurn = @{-1="L";1="R"}
-
+$RawPath = @()
 $map = @(, @())
 $ASCII.outputBuffer | % {
     $val = [char]$_
     if ($_ -eq 10) { $map += , @() }
     else { $map[-1] += $val }
     if ($val -in "<", ">", "v", "^") {
+        $initialDir = @{"^" = 0; ">" = 1; "v" = 2; "<" = 3 }
         $robot.dir = $initialDir["$val"]
         $robot.y = $map.count - 1
         $robot.X = $map[-1].count - 1
@@ -40,8 +41,6 @@ $ASCII.outputBuffer | % {
 }
 $width = $map[0].Count
 $height = $map.Count - 2 # Couple of blank lines at the end...?
-
-$RawPath = @()
 
 # Work out initial turn to align with scaffold.
 #   Based on the problem, this will always be straight, left, or right
@@ -58,9 +57,72 @@ if($turn -ne 0){
 }
 
 # Walk the scaffold, only turning when you have to (i.e. go straight through intersections)
+$ForwardXYDelta = @{
+    "^" = @(0,-1);  0 = @(0,-1);
+    ">" = @(1,0);   1 = @(1,0); 
+    "v" = @(0,1);   2 = @(0,1); 
+    "<" = @(-1,0);  3 = @(-1,0); 
+}#x,y
+function Can-Walk-Forward{
+    $delta = $ForwardXYDelta[$robot.dir]
+    $newXY = ($robot.x+$delta[0]),($robot.y+$delta[1])
+    if($newXY -lt 0){$false; return}
+    if($newXY[0] -ge $width){$false; return}
+    if($newXY[1] -ge $height){$false; return}
+    if($map[$newXY[1]][$newXY[0]] -ne "#"){$false;return}
+    $true;
+}
+
+function Turn-Direction{
+    $leftTileDelta = $ForwardXYDelta[(($robot.dir+3) % 4)]
+    $leftTileCoords = ($robot.x+$leftTileDelta[0]),($robot.y+$leftTileDelta[1])
+    if(-1 -notin $leftTileCoords){
+        $leftTile = $map[$leftTileCoords[1]][$leftTileCoords[0]]
+        if($leftTile -eq "#"){3; return}
+    }
+    $rightTileDelta = $ForwardXYDelta[(($robot.dir+1) % 4)]
+    $rightTileCoords = ($robot.x+$rightTileDelta[0]),($robot.y+$rightTileDelta[1])
+    if(-1 -notin $rightTileCoords){
+        $rightTile = $map[$rightTileCoords[1]][$rightTileCoords[0]]
+        if($rightTile -eq "#"){1; return}
+    }
+    0 # No turn, we're at the end
+}
+
+do{
+    # Walk forward as long as we can
+    $steps = 0
+    while(Can-Walk-Forward){
+        $steps++
+        $delta = $ForwardXYDelta[$robot.dir]
+        $robot.x+=$delta[0]
+        $robot.y+=$delta[1]
+    }
+    $RawPath+=$steps.ToString()
+
+    # Check which direction we need to turn
+    $turn = Turn-Direction
+    if($turn -ne 0){
+        $RawPath += $turn -eq 1 ? "R" : "L"
+        $robot.dir = ($robot.dir + 4 + $turn ) % 4
+    }
+}while($turn -ne 0)
+
 
 # Spit out the directions for now for me to look at
+write-host $RawPath
 
+<#
+L 8 R 12 R 12 R 10 R 10 R 12 R 10 L 8 R 12 R 12 R 10 R 10 R 12 R 10 L 10 R 10 L 6 L 10 R 10 L 6 R 10 R 12 R 10 L 8 R 12 R 12 R 10 R 10 R 12 R 10 L 10 R 10 L 6
+
+This could be an interesting compression problem, but by hand:
+
+A = L,8,R,12,R,12,R,10
+B = R,10,R,12,R,8
+C = L,10,R,10,L6
+
+
+#>
 
 
 # }
