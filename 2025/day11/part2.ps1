44bb44e85509acce53e00d121dfa40a5be00063a
CultOfPartiality <jason@wridgway.net>
2025-12-11 19:28:00 +1100

    Close!

--- 2025/day11/part2.ps1
+++ 2025/day11/part2.ps1
@@ -12,21 +12,34 @@ function Solution {
     get-content $Path | % {
         $split = $_ -replace ":", "" -split " "
         $Servers[$split[0]] = [PSCustomObject]@{
-            Name            = $split[0]
-            ConnectionNames = [array]($split | select -Skip 1)
-            Connections     = @()
+            Name               = $split[0]
+            ConnectionNames    = [array]($split | select -Skip 1)
+            ConnectionsTo      = @()
+            ConnectedFromNames = @()
+            ConnectedFrom      = @()
+            JumpsFromDac       = 0
+            PathsFromDac = 0
         }
+    }
+    $Servers["out"] = [PSCustomObject]@{
+        Name               = "out"
+        ConnectionNames    = @()
+        ConnectionsTo      = @()
+        ConnectedFromNames = @()
+        ConnectedFrom      = @()
     }
     # $graph = ""
     foreach ($server in $Servers.Values) {
         foreach ($ConnectionName in $server.ConnectionNames) {
-            $server.Connections += $Servers[$ConnectionName]
+            $server.ConnectionsTo += $Servers[$ConnectionName]
+            $Servers[$ConnectionName].ConnectedFromNames += $server.Name
+            $Servers[$ConnectionName].ConnectedFrom += $server
             # $graph += "`t$($server.Name) -> $ConnectionName;`n"
         }
     }
 
     # "digraph G{
-	# `n"+
+    # `n"+
     # ($graph -join "`n") +
     # "	{}"+
     # "}" | Out-File -FilePath "$PSScriptRoot\vis.dot"
@@ -38,62 +51,40 @@ function Solution {
     # From inspection, the path needs to hit fft first, then dac, then out
     # Looks like there are no loops
 
+
+    # Actually walk this one in reverse
     $svr_to_fft = 0
     $mostJumps = 10
-    $searchSpace = new-object 'System.Collections.Stack'
-    $searchSpace.Push(
+    $searchSpace = new-object 'System.Collections.Queue'
+    $searchSpace.Enqueue(
         [PSCustomObject]@{
-            CurrentNode = "svr"
-            Jumps = 0
+            CurrentNode = "fft"
+            Jumps       = 0
         })
     while ($searchSpace.Count) {
-        $state = $searchSpace.Pop()
-        foreach ($nextStep in $Servers[$state.CurrentNode].ConnectionNames) {
-            if ($nextStep -eq "fft") {
+        $state = $searchSpace.Dequeue()
+        foreach ($nextStep in $Servers[$state.CurrentNode].ConnectedFromNames) {
+            if ($nextStep -eq "svr") {
                 $svr_to_fft++
                 continue
             }
             $nextState = [PSCustomObject]@{
                 CurrentNode = $nextStep
-                Jumps = $state.Jumps++
+                Jumps       = $state.Jumps + 1
             }
-            if($nextState.Jumps -ge $mostJumps){continue}
-            $searchSpace.Push($nextState)
-        }
-    }
-    write-host "$svr_to_fft jumps to between svr and fft"
-
-    $fft_to_dac = 0
-    $mostJumps = 24
-    $searchSpace = new-object 'System.Collections.Queue'
-    $searchSpace.Enqueue(
-        [PSCustomObject]@{
-            CurrentNode = "fft"
-            Jumps = 0
-        })
-    while ($searchSpace.Count) {
-        $state = $searchSpace.Dequeue()
-        foreach ($nextStep in $Servers[$state.CurrentNode].ConnectionNames) {
-            if ($nextStep -eq "dac") {
-                $fft_to_dac++
-                continue
-            }
-            $nextState = [PSCustomObject]@{
-                CurrentNode = $nextStep
-                Jumps = $state.Jumps++
-            }
-            if($nextState.Jumps -gt $mostJumps){continue}
+            if ($nextState.Jumps -ge $mostJumps) { continue }
             $searchSpace.Enqueue($nextState)
         }
     }
-    write-host "$fft_to_dac jumps to between fft and dac"
+    write-host "$svr_to_fft distinct paths between svr and fft"
 
+    # Lower third down to the end
     $dac_to_out = 0
     $searchSpace = new-object 'System.Collections.Queue'
     $searchSpace.Enqueue(
         [PSCustomObject]@{
             CurrentNode = "dac"
-            Jumps = 0
+            Jumps       = 0
         })
     while ($searchSpace.Count) {
         $state = $searchSpace.Dequeue()
@@ -104,12 +95,63 @@ function Solution {
             }
             $nextState = [PSCustomObject]@{
                 CurrentNode = $nextStep
-                Jumps = $state.Jumps++
+                Jumps       = $state.Jumps + 1
             }
             $searchSpace.Enqueue($nextState)
         }
     }
-    write-host "$dac_to_out jumps to between dac and out"
+    write-host "$dac_to_out distinct paths between dac and out"
+
+    # And finally the messy middle.........
+    # First set the JumpsFromDac for relevant entries
+    $mostJumps = 19
+    $searchSpace = new-object 'System.Collections.Stack'
+    $searchSpace.Push(
+        [PSCustomObject]@{
+            CurrentNode = "dac"
+            Jumps       = 0
+        })
+    while ($searchSpace.Count) {
+        $state = $searchSpace.Pop()
+        foreach ($nextStep in $Servers[$state.CurrentNode].ConnectedFromNames) {
+            if($Servers[$nextStep].JumpsFromDac -gt $state.Jumps){continue}
+            $nextState = [PSCustomObject]@{
+                CurrentNode = $nextStep
+                Jumps       = $state.Jumps + 1
+            }
+            $Servers[$nextStep].JumpsFromDac = $nextState.Jumps
+            if ($nextState.Jumps -gt $mostJumps) { continue }
+            $searchSpace.Push($nextState)
+        }
+    }
+
+
+    $fft_to_dac = 0
+    $mostJumps = 19
+    $searchSpace = new-object 'System.Collections.Queue'
+    $searchSpace.Enqueue(
+        [PSCustomObject]@{
+            CurrentNode = "fft"
+            Jumps       = 0
+        })
+    while ($searchSpace.Count) {
+        $state = $searchSpace.Dequeue()
+        foreach ($nextStep in $Servers[$state.CurrentNode].ConnectionNames) {
+            if ($nextStep -eq "dac") {
+                $fft_to_dac++
+                continue
+            }
+            $nextState = [PSCustomObject]@{
+                CurrentNode = $nextStep
+                Jumps       = $state.Jumps + 1
+            }
+            if ($nextState.Jumps -gt $mostJumps) { continue }
+            $searchSpace.Enqueue($nextState)
+        }
+    }
+    write-host "$fft_to_dac distinct paths between fft and dac"
+
+
 
     $svr_to_fft * $fft_to_dac * $dac_to_out
 }
