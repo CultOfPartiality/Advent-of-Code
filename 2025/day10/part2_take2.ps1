f0a9fc5de4afe112eddef9555b54e5ebf5bce76e
jason <jason@wridgway.net>
2025-12-12 23:45:24 +1100

    Still not it......
    :(

--- 2025/day10/part2_take2.ps1
+++ 2025/day10/part2_take2.ps1
@@ -3,6 +3,7 @@
 
 #The following line is for development
 $Path = "$PSScriptRoot/testcases/test1.txt"
+$Path = "$PSScriptRoot/input.txt"
 
 function Solution {
     param ($Path)
@@ -19,8 +20,9 @@ function Solution {
         }
     }
 
-    $machines | Foreach-Object -ThrottleLimit 20 -Parallel {
-        # $machines | Foreach-Object {
+    # $machines | Foreach-Object -ThrottleLimit 20 -Parallel {
+    $result = @()
+    $machines | Foreach-Object {
 
         function GaussJordan-Elimination {
             param ($machine)
@@ -46,7 +48,6 @@ function Solution {
             # write-host "Initial matrix:"
             # $matrix | % { write-host ($_ -join ",") }
             # write-host
-
 
             $row = 0
             for ($col = 0; $col -lt $matrix[0].Count - 1; $col++) {
@@ -76,6 +77,17 @@ function Solution {
                 # write-host "Round $col"
                 # $matrix | %{ write-host ($_ -join ",") }
                 # write-host
+            }
+
+            #Fight floating point issues
+            for ($col = 0; $col -lt $matrix[0].Count; $col++) {
+                for ($row = 0; $row -lt $matrix.Count; $row++) {
+                    $val = [Math]::abs( $matrix[$row][$col] )
+                    $epsilon = [math]::abs($val - [math]::round($val))
+                    if ( $epsilon -lt 1E-5 ) {
+                        $matrix[$row][$col] = [Math]::Round($matrix[$row][$col])
+                    }
+                }
             }
 
             # #Debug
@@ -119,6 +131,16 @@ function Solution {
                 $knownElements[$index] = $row[-1]
                 foreach ($i in $otherIndexes) { $knownElements[$index] -= $row[$i] }
             }
+
+            #Fight floating point issues
+            for ($i = 0; $i -lt $knownElements.Count; $i++) {
+                $val = [Math]::abs( $knownElements[$i] )
+                $epsilon = [math]::abs($val - [math]::round($val))
+                if ( $epsilon -lt 1E-5 ) {
+                    $knownElements[$i] = [Math]::Round($knownElements[$i])
+                }
+            }
+
             $valid = ($knownElements -lt 0).count -eq 0
             return @($valid, (($knownElements | measure -Sum).Sum))
         }
@@ -127,78 +149,121 @@ function Solution {
         # $machine = $machines[0]
         $matrix, $definedElements, $freeElements = GaussJordan-Elimination $machine
         # write-host ("Defined Elements: " + ($definedElements -join ","))
-        # write-host ("Free Elements: " + ($freeElements -join ","))
+        write-host "Machine {$($machine.Counters -join ",")}"
+        $matrix | % { write-host (($_ | % { "$_".PadLeft(3, " ") }) -join ",") }; write-host
+        write-host ("Free Elements: " + ($freeElements -join ","))
 
         #TODO Need to optimise free elements, without another element turning out less than zero
         $knownElements = @($null) * ($matrix[0].Count - 1)
         # $freeElements | % { $knownElements[$_] = 0 }
 
-        $fewestPresses = [int32]::MaxValue
-        for ($i = 0; $i -le ($freeElements.Count -gt 0 ? 150 : 0); $i++) {
-            for ($j = 0; $j -le ($freeElements.Count -gt 1 ? 80 : 0); $j++) {
-                for ($k = 0; $k -le ($freeElements.Count -gt 2 ? 60 : 0); $k++) {
-                    $options = $i, $j, $k
-                    $knownElements = @($null) * ($matrix[0].Count - 1)
-                    if ($freeElements.Count) {
-                        0..($freeElements.Count - 1) | % { $knownElements[$freeElements[$_]] = $options[$_] }
-                    }
-                    $valid, $total = Sum-Matrix -matrixInput $matrix -knownElements $knownElements
-                    if ($valid) {
-                        $fewestPresses = [Math]::Min($fewestPresses, $total)
-                    }
+        $state = [PSCustomObject]@{
+            freeElementIndexes = $freeElements
+            freeElementValues  = @(0) * $freeElements.Count
+            total              = [int32]::MaxValue
+            valid              = $false
+            prevValid          = $false
+            prevTotal          = [int32]::MaxValue
+        }
+        $knownElements = @($null) * ($matrix[0].Count - 1)
+        if ($state.freeElementIndexes.Count) {
+            0..($state.freeElementIndexes.count - 1) | % {
+                $index = $state.freeElementIndexes[$_]
+                $knownElements[$index] = $state.freeElementValues[$_]
+            }
+        }
+        $state.valid, $state.total = Sum-Matrix -matrixInput $matrix -knownElements $knownElements
+    
+        $statesSeen = @{}
+        $searchSpace = New-Object 'System.Collections.Generic.PriorityQueue[psobject,int]'
+        if ($state.freeElementIndexes.Count) {
+            # Don't populate if we've got no free elements; we're done
+            $searchSpace.Enqueue($state, 0)
+            $minPushes = ($state.valid -and $state.total -eq [Math]::floor($state.total) )? $state.total : [int32]::MaxValue
+            $anyValidOptionFound = ($state.valid -and $state.total -eq [Math]::floor($state.total) )
+        }
+        else {
+            $minPushes = $state.total
+        }
+        $statesSeen[$state.freeElementValues -join ","] = 1
+        write-host "`tStarting total: $minPushes"
+    
+        while ($searchSpace.Count) {
+            $state = $searchSpace.Dequeue()
+            for ($freeIndex = 0; $freeIndex -lt $state.freeElementIndexes.Count; $freeIndex++) {
+                $newState = [PSCustomObject]@{
+                    freeElementIndexes = $state.freeElementIndexes.clone()
+                    freeElementValues  = $state.freeElementValues.clone()
+                    total              = $null
+                    valid              = $null
+                    prevValid          = $state.valid
+                    prevTotal          = $state.total
+                }
+                $newState.freeElementValues[$freeIndex]++
+                if ($statesSeen.ContainsKey($newState.freeElementValues -join ",")) { continue }
+                $statesSeen[$newState.freeElementValues -join ","] = 1
+                $knownElements = @($null) * ($matrix[0].Count - 1)
+                0..($newState.freeElementIndexes.count - 1) | % {
+                    $index = $newState.freeElementIndexes[$_]
+                    $knownElements[$index] = $newState.freeElementValues[$_]
+                }
+                $newState.valid, $newState.total = Sum-Matrix -matrixInput $matrix -knownElements $knownElements
+                # write-host "Button pushes: ($($knownElements -join ",")) = $total"
+
+                $priority = ( $newState.valid ? [math]::Abs($newState.total) : ($knownElements | ? { $_ -lt 0 } | % { - $_ } | sum-array ) )
+                # if no valid options have been found, keep going
+                if (!$anyValidOptionFound) {
+                    $searchSpace.Enqueue($newState, $priority)
+                }
+                # if fractional, the total's still valid, keep going
+                if(  $newState.total -ne [Math]::Floor($newState.total) -and $newState.valid ){
+                    $searchSpace.Enqueue($newState, $priority)
+                }
+                # if previously valid and now not valid, don't go again
+                # if previously not valid and now valid, store and go again
+                elseif (!$newState.prevValid -and $newState.valid) {
+                    $searchSpace.Enqueue($newState, $priority)
+                }
+                # if previously valid and still valid, and the total has gone down or is the same, store and go again
+                elseif (
+                    # $newState.prevValid -and
+                    # $newState.valid -and
+                    ($newState.prevValid -or $newState.valid) -and
+                    ($newState.total -le ($newState.prevTotal + 5) <#-or $newstate.total -le ($minPushes + 20)#>)
+                    ) {
+                    $searchSpace.Enqueue($newState, $priority)
+                }
+                elseif(($knownElements| Measure-Object -Minimum).Minimum -gt -50 -and $newstate.total -le ($minPushes + 20)){
+                    $searchSpace.Enqueue($newState, $priority)
+                }
+
+                $anyValidOptionFound = $anyValidOptionFound -or ($newState.valid -and $newState.total -eq [Math]::Floor($newState.total))
+
+                if ($newstate.valid -and
+                    $newState.total -lt $minPushes -and
+                    $newState.total -eq [Math]::Floor($newState.total) ) {
+                    $minPushes = $newState.total
+                    write-host "`tNew min total: $minPushes"
                 }
             }
         }
-        if ($fewestPresses -eq [int32]::MaxValue) {
-            write-host ("Error, need more checks. Free Elements: " + ($freeElements -join ","))
+        write-host "`t`tFinal min total: $minPushes"
+
+        if (($minPushes -ne [math]::floor($minPushes))) {
+            write-host "Error, result is a fractional button press..."  -ForegroundColor Red
             exit
         }
-
-        # $valid, $total = Sum-Matrix -matrixInput $matrix -knownElements $knownElements
-        # if (!$valid) {
-        #     write-host "Error"
-        #     exit
-        # }
-        # $state = [PSCustomObject]@{
-        #     freeElementIndexes = $freeElements
-        #     freeElementValues  = @(0) * $freeElements.Count
-        #     total              = $total
-        # }
-        #
-        # $searchSpace = New-Object 'System.Collections.Stack'
-        # $searchSpace.Push($state)
-        # $minPushes = $state.total
-        #
-        # while ($searchSpace.Count) {
-        #     $state = $searchSpace.Pop()
-        #     for ($freeIndex = 0; $freeIndex -lt $state.freeElementIndexes.Count; $freeIndex++) {
-        #         $newState = [PSCustomObject]@{
-        #             freeElementIndexes = $state.freeElementIndexes.clone()
-        #             freeElementValues  = $state.freeElementValues.clone()
-        #             total              = $null
-        #         }
-        #         $newState.freeElementValues[$freeIndex]++
-        #         $knownElements = @($null) * ($matrix[0].Count - 1)
-        #         0..($newState.freeElementIndexes.count - 1) | % {
-        #             $index = $newState.freeElementIndexes[$_]
-        #             $value = $newState.freeElementValues[$_]
-        #             $knownElements[$index] = $value
-        #         }
-        #         $valid, $newState.total = Sum-Matrix -matrixInput $matrix -knownElements $knownElements
-        #         # write-host "Button pushes: ($($knownElements -join ",")) = $total"
-        #         if ($valid -and $newState.total -le $minPushes) {
-        #             $minPushes = $newState.total
-        #             write-host "`tNew min total: $minPushes"
-        #             $searchSpace.Push($newState)
-        #         }
-        #     }
-        # }
-        # write-host "`t`tFinal min total: $minPushes"
+        if ($minPushes -eq [int32]::MaxValue) {
+            write-host "Error, valid solution not found..." -ForegroundColor Red
+            exit
+        }
     
-        $fewestPresses
-    } | sum-array
+        $result += $minPushes
+    }
+    $result | sum-array
 }
 Unit-Test  ${function:Solution} "$PSScriptRoot/testcases/test1.txt" 33
 $measuredTime = measure-command { $result = Solution "$PSScriptRoot\input.txt" }
 Write-Host "Part 2: $result`nExecution took $($measuredTime.TotalSeconds)s" -ForegroundColor Magenta
-write-host "55834588098 is too high" -ForegroundColor Yellow
+if($result -ge 16385){write-host "$result is too high" -ForegroundColor Red}
+write-host "Not 16366 or 16359" -ForegroundColor Yellow
